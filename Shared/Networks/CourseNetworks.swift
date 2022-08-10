0fd6682fe224b04377c44041d4426e43ffdecd34
fsy2001 <fsy2001@outlook.com>
2022-08-10 22:32:53 +0800

    add course local cache

--- Shared/Networks/CourseNetworks.swift
+++ Shared/Networks/CourseNetworks.swift
@@ -5,10 +5,21 @@ import Foundation
 let DANKE_BASE_URL = "https://danke.fduhole.com/api"
 
 extension NetworkRequests {
-    func loadCourseGroups() async throws -> [DKCourseGroup] {
+    func loadCourseGroups() async throws -> ([DKCourseGroup], Data) {
         let components = URLComponents(string: DANKE_BASE_URL + "/courses")!
         let data = try await networkRequest(url: components.url!)
         let decodedResponse = try JSONDecoder().decode([DKCourseGroup].self, from: data)
-        return decodedResponse
+        return (decodedResponse, data)
+    }
+    
+    func loadCourseHash() async throws -> String {
+        struct Hash: Codable {
+            let hash: String
+        }
+        
+        let components = URLComponents(string: DANKE_BASE_URL + "/courses/hash")!
+        let data = try await networkRequest(url: components.url!)
+        let hash = try JSONDecoder().decode(Hash.self, from: data)
+        return hash.hash
     }
 }
--- Shared/Pages/CourseMainPage.swift
+++ Shared/Pages/CourseMainPage.swift
@@ -1,13 +1,32 @@
 import SwiftUI
 
 struct CourseMainPage: View {
+    @AppStorage("course-data") var courseData = Data() // cache data
+    @AppStorage("course-hash") var courseHash = ""
     @State var courses: [DKCourseGroup] = []
     
-    func loadCourse() async {
-        do {
-            courses = try await networks.loadCourseGroups()
+    func initialLoad() async {
+        print("DANXI-DEBUG: course hash \(courseHash)")
+        
+        do { // check hash, try decode from local storage
+            let newHash = try await networks.loadCourseHash()
+            if newHash == courseHash { // no change from last fetch, use local storage
+                print("DANXI-DEBUG: using local storage")
+                courses = try JSONDecoder().decode([DKCourseGroup].self, from: courseData)
+                return
+            } else {
+                courseHash = newHash // changed
+            }
         } catch {
-            print("DANXI-DEBUG: load course group failed")
+            print("DANXI-DEBUG: check local storage failed")
+        }
+        
+        do {
+            (courses, courseData) = try await networks.loadCourseGroups()
+            print("DANXI-DEBUG: making network call")
+        } catch {
+            print(error)
+            print("DANXI-DEBUG: initial load failed")
         }
     }
     
@@ -20,7 +39,7 @@ struct CourseMainPage: View {
         .navigationTitle("Curriculum Board")
         .listStyle(.grouped)
         .task {
-            await loadCourse()
+            await initialLoad()
         }
         
     }
