5baee561df61d412cb7fcf8d9cdfe8fe496c4cc7
fsy2001 <fsy2001@outlook.com>
2022-08-16 21:29:52 +0800

    remove renderer debug print statement

--- Shared/Components/RendererViews.swift
+++ Shared/Components/RendererViews.swift
@@ -32,10 +32,6 @@ class PlaceHolderView: UIView {
 
 class CustomViewRendererProvider: NSTextAttachmentViewProvider {
     override func loadView() {
-        if let data = textAttachment?.contents {
-            // Data for this view. Use this to determine what the view should be
-            print(String(data: data, encoding: .utf8))
-        }
         view = PlaceHolderView()
         view?.backgroundColor = .purple
         (view as? PlaceHolderView)?.animate()
