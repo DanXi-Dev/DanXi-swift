cbe1d6768254130da90df344cf1e1cf280a1192b
fsy2001 <fsy2001@outlook.com>
2022-08-29 17:55:27 +0800

    apply init loading delegate to course main page

--- Shared/Pages/CourseMainPage.swift
+++ Shared/Pages/CourseMainPage.swift
@@ -4,15 +4,23 @@ struct CourseMainPage: View {
     @AppStorage("course-hash") var courseHash = ""
     @State var courses: [DKCourseGroup]
     
+    @State var searchText = ""
+    
+    @State var loading = true
+    @State var failed = false
+    @State var errorInfo = ErrorInfo()
+    
+    
     init() {
         self._courses = State(initialValue: [])
     }
     
-    init(courses: [DKCourseGroup]) {
+    init(courses: [DKCourseGroup]) { // preview purpose
         self._courses = State(initialValue: courses)
+        self._loading = State(initialValue: false)
     }
     
-    @State var searchText = ""
+    
     var searchResults: [DKCourseGroup] {
         if searchText.isEmpty {
             return courses
@@ -32,26 +40,31 @@ struct CourseMainPage: View {
                 return
             }
         } catch {
-            print("DANXI-DEBUG: check local storage failed")
+            
         }
         
         do { // make network call
             courses = try await NetworkRequests.shared.loadCourseGroups()
             saveDKCourseList(courses)
-            courseHash = newHash // defer update course hash to prevent loading course fail
+            courseHash = newHash // defer update course hash to prevent hash-content inconsistent
+        } catch NetworkError.ignore {
+            // cancelled, ignore
+        } catch let error as NetworkError {
+            failed = true
+            errorInfo = error.localizedErrorDescription
         } catch {
-            print("DANXI-DEBUG: initial load failed")
+            failed = true
+            errorInfo = ErrorInfo(title: "Unknown Error",
+                                  description: "Error description: \(error.localizedDescription)")
         }
     }
     
     var body: some View {
-        if (courses.isEmpty) {
-            ProgressView()
-                .navigationTitle("Curriculum Board")
-                .task {
-                    await initialLoad()
-                }
-        } else {
+        InitLoadingView(loading: $loading,
+                        failed: $failed,
+                        errorDescription: errorInfo.description) {
+            await initialLoad()
+        } content: {
             List {
                 ForEach(searchResults) { course in
                     NavigationLink(destination: CourseDetailPage(courseGroup: course)) {
@@ -68,8 +81,15 @@ struct CourseMainPage: View {
 
 struct CourseMainPage_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            CourseMainPage(courses: PreviewDecode.decodeList(name: "course-list"))
+        Group {
+            NavigationView {
+                CourseMainPage(courses: PreviewDecode.decodeList(name: "course-list"))
+            }
+            
+            NavigationView {
+                CourseMainPage(courses: PreviewDecode.decodeList(name: "course-list"))
+            }
+            .preferredColorScheme(.dark)
         }
     }
 }
