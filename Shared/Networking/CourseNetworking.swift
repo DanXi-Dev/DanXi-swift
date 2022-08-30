9e6a58418ec7605a8e40b3332e97c69ee5d2ba10
fsy2001 <fsy2001@outlook.com>
2022-08-31 01:39:16 +0800

    implement course review API

--- Shared/Networking/CourseNetworking.swift
+++ Shared/Networking/CourseNetworking.swift
@@ -5,6 +5,9 @@ import Foundation
 let DANKE_BASE_URL = "https://danke.fduhole.com/api"
 
 extension NetworkRequests {
+    
+    // MARK: course info
+    
     func loadCourseGroups() async throws -> [DKCourseGroup] {
         let components = URLComponents(string: DANKE_BASE_URL + "/courses")!
         return try await requestObj(url: components.url!)
@@ -24,5 +27,37 @@ extension NetworkRequests {
     func loadCourseGroup(id: Int) async throws -> DKCourseGroup {
         let components = URLComponents(string: DANKE_BASE_URL + "/group/\(id)")!
         return try await requestObj(url: components.url!)
+    }
+    
+    // MARK: review
+    
+    func review(courseId: Int, content: String, title: String, rank: DKRank, modify: Bool = false) async throws -> DKReview {
+        struct ReviewObj: Codable {
+            let title, content: String
+            let rank: DKRank
+        }
+        
+        let payload = ReviewObj(title: title, content: content, rank: rank)
+        let payloadData = try JSONEncoder().encode(payload)
+        
+        let components = URLComponents(string: DANKE_BASE_URL + "/course/\(courseId)/reviews")!
+        return try await requestObj(url: components.url!, data: payloadData, method: modify ? "PUT" : "POST")
+    }
+    
+    func myReviews() async throws -> [DKReview] {
+        let components = URLComponents(string: DANKE_BASE_URL + "/reviews/me")!
+        return try await requestObj(url: components.url!)
+    }
+    
+    func upvote(reviewId: Int, upvote: Bool) async throws -> DKReview {
+        struct UpvoteConfig: Codable {
+            let upvote: Bool
+        }
+        
+        let payload = UpvoteConfig(upvote: upvote)
+        let payloadData = try JSONEncoder().encode(payload)
+        
+        let components = URLComponents(string: DANKE_BASE_URL + "/reviews/\(reviewId)")!
+        return try await requestObj(url: components.url!, data: payloadData, method: "PATCH")
     }
 }
