8ac36617f52909c365bb26ee439f61b198d9eea9
fsy2001 <fsy2001@outlook.com>
2022-10-11 00:48:23 +0800

    update navigation model

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,10 +1,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var model = TreeholeDataModel.shared
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @State var showSettingPage = false
     
     var body: some View {
-        autoAppNavigation
+        NavigationView {
+            List {
+                NavigationLink {
+                    if model.loggedIn {
+                        TreeholePage()
+                    } else {
+                        TreeholeWelcomePage()
+                    }
+                } label: {
+                    Label("Tree Hole", systemImage: "text.bubble")
+                }
+                
+                NavigationLink {
+                    if model.loggedIn {
+                        CourseMainPage()
+                    } else {
+                        CourseWelcomePage()
+                    }
+                } label: {
+                    Label("Curriculum", systemImage: "books.vertical")
+                }
+            }
+            .navigationTitle("DanXi")
+            .toolbar {
+                Button {
+                    showSettingPage = true
+                } label: {
+                    Image(systemName: "gearshape")
+                }
+            }
+            .sheet(isPresented: $showSettingPage) {
+                NavigationView { SettingsPage() }
+            }
+        }
     }
     
     @ViewBuilder
