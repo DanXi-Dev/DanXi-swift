bcca9ad980ca5b6366639fe974c504b0a572c859
fsy2001 <fsy2001@outlook.com>
2022-10-16 00:54:08 +0800

    restore floor history

--- Shared/Pages/HistoryPage.swift
+++ Shared/Pages/HistoryPage.swift
@@ -8,6 +8,12 @@ struct HistoryList: View {
     @State var initFinished = false
     @State var loadingError = ""
     
+    @State var showRestoreSheet = false
+    @State var historyId: Int? = nil
+    @State var restoreReason = ""
+    @State var showRestoreError = false
+    @State var restoreErrorInfo = ""
+    
     func loadHistory() async {
         do {
             histories = try await DXNetworks.shared.loadFloorHistory(floorId: floor.id)
@@ -15,6 +21,16 @@ struct HistoryList: View {
         } catch {
             loadingError = error.localizedDescription
         }
+    }
+    
+    func restoreFloor() async throws {
+        guard let historyId = historyId else {
+            return
+        }
+        
+        floor = try await DXNetworks.shared.restoreFloor(floorId: floor.id,
+                                                         historyId: historyId,
+                                                         restoreReason: restoreReason)
     }
     
     var body: some View {
@@ -31,7 +47,7 @@ struct HistoryList: View {
                                     .foregroundColor(.secondary)
                                     .font(.caption)
                             }
-
+                            
                             MarkdownView(history.content)
                             
                             HStack {
@@ -42,11 +58,34 @@ struct HistoryList: View {
                             .foregroundColor(.secondary)
                             .font(.caption)
                         }
+                        .swipeActions {
+                            Button {
+                                self.historyId = history.id
+                                showRestoreSheet = true
+                            } label: {
+                                Label("Restore", systemImage: "arrow.uturn.backward")
+                            }
+                        }
                     }
+                }
+                .sheet(isPresented: $showRestoreSheet) {
+                    restoreForm
                 }
             }
                         .navigationTitle("Edit History")
                         .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    var restoreForm: some View {
+        FormPrimitive(title: "Restore History",
+                      allowSubmit: true,
+                      errorTitle: "Restore Failed") {
+            Section {
+                TextField("Restore reason", text: $restoreReason)
+            }
+        } action: {
+            try await restoreFloor()
         }
     }
 }
