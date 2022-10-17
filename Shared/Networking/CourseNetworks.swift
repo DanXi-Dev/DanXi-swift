1c46ebe54c35e4139b13fcf3137ff83b2bd82c6c
fsy2001 <fsy2001@outlook.com>
2022-10-17 22:18:50 +0800

    implement curriculum board API

--- Shared/Networking/CourseNetworks.swift
+++ Shared/Networking/CourseNetworks.swift
@@ -6,10 +6,21 @@ extension DXNetworks {
     
     // MARK: Course Info
     
+    
+    /// Get all course groups.
     func loadCourseGroups() async throws -> [DKCourseGroup] {
         return try await requestObj(url: URL(string: DANKE_BASE_URL + "/courses")!)
     }
     
+    
+    /// Get course group by ID.
+    func loadCourseGroup(id: Int) async throws -> DKCourseGroup {
+        let components = URLComponents(string: DANKE_BASE_URL + "/group/\(id)")!
+        return try await requestObj(url: components.url!)
+    }
+    
+    
+    /// Get course groups hash.
     func loadCourseHash() async throws -> String {
         struct Hash: Codable {
             let hash: String
@@ -19,14 +30,41 @@ extension DXNetworks {
         return hash.hash
     }
     
-    func loadCourseGroup(id: Int) async throws -> DKCourseGroup {
-        let components = URLComponents(string: DANKE_BASE_URL + "/group/\(id)")!
-        return try await requestObj(url: components.url!)
+
+    /// Get course by ID.
+    func loadCourse(id: Int) async throws -> DKCourse {
+        return try await requestObj(url: URL(string: DANKE_BASE_URL + "/course/\(id)")!)
     }
+    
     
     // MARK: Review
     
-    func review(courseId: Int, content: String, title: String, rank: DKRank, modify: Bool = false) async throws -> DKReview {
+    
+    /// Get all reviews published by me.
+    func myReviews() async throws -> [DKReview] {
+        return try await requestObj(url: URL(string: DANKE_BASE_URL + "/reviews/me")!)
+    }
+    
+    
+    
+    /// Get all reviews on course with the given course ID.
+    /// - Parameter courseId: Course ID.
+    /// - Returns: Review list.
+    func loadReviews(courseId: Int) async throws -> [DKReview] {
+        return try await requestObj(url: URL(string: DANKE_BASE_URL + "/courses/\(courseId)/reviews")!)
+    }
+    
+    
+    
+    /// Add or modify a review
+    /// - Parameters:
+    ///   - courseId: Course ID.
+    ///   - content: Review content.
+    ///   - title: Review title.
+    ///   - rank: Rank struct, as in `DKRank`.
+    ///   - modify: Whether to post new or modify existing review.
+    /// - Returns: Review struct.
+    func postReview(courseId: Int, content: String, title: String, rank: DKRank, modify: Bool = false) async throws -> DKReview {
         struct ReviewConfig: Codable {
             let title, content: String
             let rank: DKRank
@@ -37,11 +75,21 @@ extension DXNetworks {
                                     method: modify ? "PUT" : "POST")
     }
     
-    func myReviews() async throws -> [DKReview] {
-        return try await requestObj(url: URL(string: DANKE_BASE_URL + "/reviews/me")!)
+    
+    /// Remove review by ID.
+    /// - Parameter id: Review ID.
+    func removeReview(id: Int) async throws {
+        try await sendRequest(url: URL(string: DANKE_BASE_URL + "reviews/\(id)")!, method: "DELETE")
     }
     
-    func upvote(reviewId: Int, upvote: Bool) async throws -> DKReview {
+    
+    
+    /// Vote for a review.
+    /// - Parameters:
+    ///   - reviewId: Review ID.
+    ///   - upvote: Upvote or downvote.
+    /// - Returns: New review struct.
+    func voteReview(reviewId: Int, upvote: Bool) async throws -> DKReview {
         struct UpvoteConfig: Codable {
             let upvote: Bool
         }
