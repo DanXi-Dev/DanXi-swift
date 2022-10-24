410af63a3e6758f279beffe779f5b0b949fbf776
fsy2001 <fsy2001@outlook.com>
2022-10-24 22:58:42 +0800

    create dorm info entity model

---
+++ Shared/Entities/FudanEntity.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+struct DormInfo: Decodable {
+    
+    let campus: String
+    let building: String
+    let roomNo: String
+    
+    let updateTime: String
+    let usedElectricity: Float
+    let availableElectricity: Float
+    let allElectricity: Float
+    
+    enum CodingKeys: String, CodingKey {
+        case data = "d"
+        enum DataKeys: String, CodingKey {
+            case campus = "xq"
+            case building = "ssmc"
+            case roomNo = "fjmc"
+            case updateTime = "fj_update_time"
+            case usedElectricity = "fj_used"
+            case availableElectricity = "fj_surplus"
+            case allElectricity = "fj_all"
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let data = try decoder.container(keyedBy: CodingKeys.self)
+        let values = try data.nestedContainer(keyedBy: CodingKeys.DataKeys.self, forKey: .data)
+        campus = try values.decode(String.self, forKey: .campus)
+        building = try values.decode(String.self, forKey: .building)
+        roomNo = try values.decode(String.self, forKey: .roomNo)
+        updateTime = try values.decode(String.self, forKey: .updateTime)
+        usedElectricity = try values.decode(Float.self, forKey: .usedElectricity)
+        availableElectricity = try values.decode(Float.self, forKey: .availableElectricity)
+        allElectricity = try values.decode(Float.self, forKey: .allElectricity)
+    }
+}
