b33e5173ce0244f49cdf257ab75f64b6091d1c1a
fsy2001 <fsy2001@outlook.com>
2023-10-06 21:16:55 +0800

    fix: credentials not shared in app group, causing widget loading failure

--- Shared/Fudan Kit/FDModel.swift
+++ Shared/Fudan Kit/FDModel.swift
@@ -21,7 +21,7 @@ class FDModel: ObservableObject {
     
     // MARK: - Authentication
     
-    let keychain = Keychain(service: "com.fduhole.fdutools")
+    let keychain = Keychain(service: "com.fduhole.fdutools", accessGroup: "group.com.fduhole")
     @Published var isLogged: Bool = false
     var username: String? {
         didSet { keychain["username"] = username }
