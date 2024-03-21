859f74169c1c1d08773c630a982b2c5d0bdb772e
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-03-21 12:49:04 +0800

    fix: FudanKit missing dependency

--- Fudan Kit/Package.swift
+++ Fudan Kit/Package.swift
@@ -14,11 +14,14 @@ let package = Package(
             name: "FudanKit",
             targets: ["FudanKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
+    ], 
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FudanKit"),
+            name: "FudanKit", dependencies: ["SwiftSoup"]),
     ]
 )
 
