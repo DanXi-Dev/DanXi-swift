1ad4f638103d493072d5ea2bbb8f9d6855ed3456
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-03-21 23:52:27 +0800

    change: optimize gpa page

--- Shared/View/Campus/Page/FDRankPage.swift
+++ Shared/View/Campus/Page/FDRankPage.swift
@@ -10,39 +10,53 @@ struct FDRankPage: View {
             return try await FDAcademicAPI.getGPA()
         } content: { ranks in
             List {
-                let myRank = ranks.filter({ $0.isMe }).first
-                RankChart(ranks, myRank: myRank)
-                if let myRank = myRank {
-                    LabeledContent {
-                        Text(String(myRank.rank))
-                    } label: {
-                        Label("My Rank", systemImage: "number")
+                let myRank = ranks.first(where: { $0.isMe })
+                Section {
+                    if let myRank = myRank {
+                        LabeledContent {
+                            Text(String(myRank.gpa))
+                        } label: {
+                            Text("My GPA")
+                        }
+                        LabeledContent {
+                            Text(String(myRank.rank))
+                        } label: {
+                            Text("My Rank")
+                        }
+                        LabeledContent {
+                            Text(String(format: "%.1f", myRank.credit))
+                        } label: {
+                            Text("My Credit")
+                        }
                     }
-                    LabeledContent {
-                        Text(String(myRank.gpa))
+                    
+                    Button {
+                        showSheet = true
                     } label: {
-                        Label("My GPA", systemImage: "graduationcap.fill")
-                    }
-                    LabeledContent {
-                        Text(String(format: "%.1f", myRank.credit))
-                    } label: {
-                        Label("My Credit", systemImage: "person.fill")
+                        Text("Show all GPA rank")
                     }
                 }
-                Button {
-                    showSheet = true
-                } label: {
-                    Label("Show all GPA rank", systemImage: "info.circle")
+                
+                if #available(iOS 17, *) {
+                    Section("GPA Distribution") {
+                        RankChart(ranks, myRank: myRank)
+                    }
                 }
-                .sheet(isPresented: $showSheet) {
+
+            }
+            .navigationTitle("GPA Rank")
+            .navigationBarTitleDisplayMode(.inline)
+            .sheet(isPresented: $showSheet) {
+                NavigationStack {
                     List {
                         ForEach(ranks) { rank in
                             RankView(rank: rank)
                         }
                     }
+                    .navigationTitle("GPA Rank")
+                    .navigationBarTitleDisplayMode(.inline)
                 }
             }
-            .navigationTitle("GPA Rank")
         }
     }
 }
@@ -51,141 +65,113 @@ fileprivate struct RankView: View {
     let rank: FDRank
     
     var body: some View {
-        HStack {
-            Text("#\(String(rank.rank))")
-                .font(.system(.title2, design: .rounded))
-                .fontWeight(.bold)
-                .foregroundColor(.secondary)
-            if rank.isMe {
-                Image(systemName: "person.fill")
-            }
-            
-            Spacer()
-            
-            VStack(alignment: .trailing) {
-                Text(String(rank.gpa))
-                    .font(.headline)
-                Text("\(String(format: "%.1f", rank.credit)) Credit")
-                    .font(.callout)
+            HStack {
+                Text("#\(String(rank.rank))")
+                    .font(.system(.title2, design: .rounded))
+                    .fontWeight(.bold)
                     .foregroundColor(.secondary)
+                if rank.isMe {
+                    Image(systemName: "person.fill")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text(String(rank.gpa))
+                        .font(.headline)
+                    Text("\(String(format: "%.1f", rank.credit)) Credit")
+                        .font(.callout)
+                        .foregroundColor(.secondary)
+                }
             }
-        }
-        .foregroundColor(rank.isMe ? .accentColor : .primary)
+            .foregroundColor(rank.isMe ? .accentColor : .primary)
     }
 }
 
+fileprivate struct GPABin: Identifiable {
+    public var id = UUID()
+    public var gpa: Double
+    public var gpaStart: Double
+    public var gpaEnd: Double
+    public var count: Int
+    
+    init(gpaStart: Double, gpaEnd: Double, count: Int) {
+        self.gpa = (gpaStart + gpaEnd) / 2
+        self.gpaStart = gpaStart
+        self.gpaEnd = gpaEnd
+        self.count = count
+    }
+}
+
+@available(iOS 17, *)
 fileprivate struct RankChart: View {
-    private let ranks: [FDRank]
-    @State private var selectedRank: FDRank?
+    private let bins: [GPABin]
+    private let myRank: FDRank?
+    let color: Color = .accentColor
     
     init(_ ranks: [FDRank], myRank: FDRank?) {
-        // invert the label in x axis
-        self.ranks = ranks.map { rank in
-            var reversedRank = rank
-            reversedRank.rank = ranks.count - rank.rank
-            return reversedRank
-        }
-        
-        if let myRank = myRank {
-            var myRankReversed = myRank
-            myRankReversed.rank = ranks.count - myRank.rank
-            self._selectedRank = State(initialValue: myRankReversed)
-        } else {
-            self._selectedRank = State(initialValue: nil)
-        }
+        self.bins = RankChart.prepareDataForDensityChart(ranks: ranks, binWidth: 0.1)
+        self.myRank = myRank
     }
     
-    private func findElement(location: CGPoint, proxy: ChartProxy, geometry: GeometryProxy) -> FDRank? {
-        let relativeXPosition = location.x - geometry[proxy.plotAreaFrame].origin.x
-        if let rank = proxy.value(atX: relativeXPosition) as Int? {
-            // Find the closest rank element
-            var minDistance: Int = .max
-            var index: Int? = nil
-            for i in ranks.indices {
-                let distance = rank - ranks[i].rank
-                if abs(distance) < minDistance {
-                    minDistance = abs(distance)
-                    index = i
-                }
-            }
-            
-            if let index {
-                return ranks[index]
-            }
+    private var areaBackground: Gradient {
+        return Gradient(colors: [color.opacity(0.5), .clear])
+    }
+    
+    static func prepareDataForDensityChart(ranks: [FDRank], binWidth: Double) -> [GPABin] {
+        // Determine the min and max GPA to set the range of bins
+        guard let minGPA = ranks.min(by: { $0.gpa < $1.gpa })?.gpa,
+              let maxGPA = ranks.max(by: { $0.gpa < $1.gpa })?.gpa else {
+            return []
         }
-        return nil
+
+        // Calculate the number of bins needed based on the binWidth
+        let binsCount = Int(((maxGPA - minGPA) / binWidth).rounded(.up))
+        
+        // Initialize the bins
+        var bins = Array(repeating: 0, count: binsCount)
+        
+        // Assign ranks to bins
+        for rank in ranks {
+            let binIndex = Int((rank.gpa - minGPA) / binWidth)
+            bins[min(binIndex, binsCount - 1)] += 1
+        }
+        
+        // Convert bins
+        let binnedData = bins.enumerated().map { index, count in
+            let rangeStart = minGPA + binWidth * Double(index)
+            let rangeEnd = rangeStart + binWidth
+            return GPABin(gpaStart: rangeStart, gpaEnd: rangeEnd, count: count)
+        }
+        
+        return binnedData.filter({ i in i.count > 0})
+    }
+    
+    private func getBarMarkForegroundStyle(bin: GPABin) -> Color {
+        guard let myRank else {
+            return color
+        }
+        
+        if bin.gpaStart <= myRank.gpa && myRank.gpa <= bin.gpaEnd {
+            return color
+        }
+        return .secondary
     }
     
     var body: some View {
-        Chart(ranks) { rank in
-            LineMark(x: .value("Rank", rank.rank),
-                     y: .value("GPA", rank.gpa))
+        Chart(bins) { bin in
+            BarMark(
+                x: .value("GPA", bin.gpa),
+                y: .value("Density", bin.count)
+            )
+//            .foregroundStyle(getBarMarkForegroundStyle(bin: bin))
+            .foregroundStyle(color)
         }
-        .chartXAxis(.hidden)
-        .chartOverlay { proxy in
-            GeometryReader { geo in
-                Rectangle().fill(.clear).contentShape(Rectangle())
-                    .gesture(
-                        SpatialTapGesture()
-                            .onEnded { value in
-                                let rank = findElement(location: value.location, proxy: proxy, geometry: geo)
-                                if selectedRank?.rank == rank?.rank {
-                                    selectedRank = nil
-                                } else {
-                                    selectedRank = rank
-                                }
-                            }
-                            .exclusively(
-                                before: DragGesture()
-                                    .onChanged { value in
-                                        selectedRank = findElement(location: value.location, proxy: proxy, geometry: geo)
-                                    }
-                            )
-                    )
-            }
-        }
-        .chartBackground { proxy in
-            ZStack(alignment: .bottomLeading) {
-                GeometryReader { geo in
-                    if let selectedRank = selectedRank {
-                        let startPositionX1 = proxy.position(forX: selectedRank.rank) ?? 0
-                        let lineX = startPositionX1 + geo[proxy.plotAreaFrame].origin.x
-                        let lineHeight = geo[proxy.plotAreaFrame].maxY
-                        let boxWidth: CGFloat = 80
-                        let boxOffset = max(0, min(geo.size.width - boxWidth, lineX - boxWidth / 2))
-                        
-                        Rectangle()
-                            .fill(.red)
-                            .frame(width: 2, height: lineHeight)
-                            .position(x: lineX, y: lineHeight / 2)
-                        
-                        VStack(alignment: .leading) {
-                            Group {
-                                Text("GPA: \(String(format: "%.2f", selectedRank.gpa))")
-                                Text("Credit: \(String(format: "%.1f", selectedRank.credit))")
-                            }
-                            .font(.footnote)
-                            .foregroundStyle(.secondary)
-                            Text("#\(ranks.count - selectedRank.rank)") // change the reversed rank back to original
-                                .font(.title2.bold())
-                                .foregroundColor(.primary)
-                        }
-                        .frame(width: boxWidth, alignment: .leading)
-                        .background {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 8)
-                                    .fill(Color.secondarySystemBackground)
-                                RoundedRectangle(cornerRadius: 8)
-                                    .fill(.quaternary.opacity(0.7))
-                            }
-                            .padding(.horizontal, -8)
-                            .padding(.vertical, -4)
-                        }
-                        .offset(x: boxOffset)
-                    }
-                }
-            }
-        }
+        .chartXVisibleDomain(length: 3)
+        .chartScrollPosition(initialX: 1)
+        .chartScrollableAxes(.horizontal)
+        .chartXAxisLabel("GPA")
         .frame(height: 300)
+        .padding(.top, 10)
     }
 }
