1d75a1bf4c5af0c9e6b8a4877179e5a7332fbdb4
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-03-21 14:01:02 +0800

    refactor: split chart into seperate view

--- Shared/View/Campus/Page/FDElectricityPage.swift
+++ Shared/View/Campus/Page/FDElectricityPage.swift
@@ -3,12 +3,6 @@ import Charts
 import FudanKit
 
 struct FDElectricityPage: View {
-    @State private var chartSelection: Date?
-    
-    private var areaBackground: Gradient {
-        return Gradient(colors: [.green.opacity(0.5), .clear])
-    }
-    
     var body: some View {
         AsyncContentView(animation: .default) {
             async let usage = ElectricityStore.shared.getCachedElectricityUsage()
@@ -41,56 +35,70 @@ struct FDElectricityPage: View {
                 }
                 
                 if #available(iOS 17, *) {
-                    Section("Electricity Usage History") {
-                        Chart {
-                            ForEach(transactions) { d in
-                                LineMark(
-                                    x: .value("Date", d.date, unit: .day),
-                                    y: .value("", d.value)
-                                )
-                                
-                                if let chartSelection {
-                                    RuleMark(x: .value("Day", chartSelection, unit: .day))
-                                        .lineStyle(StrokeStyle(lineWidth: 1))
-                                        .foregroundStyle(.secondary)
-                                        .annotation(
-                                            position: .top, spacing: 0,
-                                            overflowResolution: .init(
-                                                x: .disabled,
-                                                y: .disabled
-                                            )
-                                        ) {
-                                            let selectionValue = transactions.first(where: {element in Calendar.current.isDate(element.date, inSameDayAs: chartSelection)})?.value ?? 0
-                                            let selectionDate = chartSelection.formatted(date: .abbreviated, time: .omitted)
-                                            
-                                            Text("\(selectionDate) \(String(format: "%.2f", selectionValue)) kWh")
-                                                .foregroundStyle(.green)
-                                                .font(.system(.body, design: .rounded))
-                                                .padding(.bottom, 4)
-                                        }
-                                }
-                                
-                                AreaMark(
-                                    x: .value("Date", d.date, unit: .day),
-                                    y: .value("", d.value)
-                                )
-                                .foregroundStyle(areaBackground)
-                            }
-                        }
-                        .chartXAxis {
-                            AxisMarks(values: .stride(by: .day, count: 2)) { _ in
-                                AxisValueLabel(format: .dateTime.day(), centered: true)
-                            }
-                        }
-                        .frame(height: 200)
-                        .chartXSelection(value: $chartSelection)
-                        .foregroundColor(.green)
-                    }
-                    .padding(.top, 20) // Leave space for annotation
+                    FDElectricityPageChart(data: transactions)
                 }
             }
             .navigationTitle("Dorm Electricity")
             .navigationBarTitleDisplayMode(.inline)
         }
+    }
+}
+
+@available(iOS 17.0, *)
+struct FDElectricityPageChart: View {
+    let data: [FDDateValueChartData]
+    @State private var chartSelection: Date?
+    
+    private var areaBackground: Gradient {
+        return Gradient(colors: [.green.opacity(0.5), .clear])
+    }
+    
+    var body: some View {
+        Section("Electricity Usage History") {
+            Chart {
+                ForEach(data) { d in
+                    LineMark(
+                        x: .value("Date", d.date, unit: .day),
+                        y: .value("", d.value)
+                    )
+                    
+                    if let chartSelection {
+                        RuleMark(x: .value("Day", chartSelection, unit: .day))
+                            .lineStyle(StrokeStyle(lineWidth: 1))
+                            .foregroundStyle(.secondary)
+                            .annotation(
+                                position: .top, spacing: 0,
+                                overflowResolution: .init(
+                                    x: .disabled,
+                                    y: .disabled
+                                )
+                            ) {
+                                let selectionValue = data.first(where: {element in Calendar.current.isDate(element.date, inSameDayAs: chartSelection)})?.value ?? 0
+                                let selectionDate = chartSelection.formatted(date: .abbreviated, time: .omitted)
+                                
+                                Text("\(selectionDate) \(String(format: "%.2f", selectionValue)) kWh")
+                                    .foregroundStyle(.green)
+                                    .font(.system(.body, design: .rounded))
+                                    .padding(.bottom, 4)
+                            }
+                    }
+                    
+                    AreaMark(
+                        x: .value("Date", d.date, unit: .day),
+                        y: .value("", d.value)
+                    )
+                    .foregroundStyle(areaBackground)
+                }
+            }
+            .chartXAxis {
+                AxisMarks(values: .stride(by: .day, count: 2)) { _ in
+                    AxisValueLabel(format: .dateTime.day(), centered: true)
+                }
+            }
+            .frame(height: 200)
+            .chartXSelection(value: $chartSelection)
+            .foregroundColor(.green)
+        }
+        .padding(.top, 20) // Leave space for annotation
     }
 }
