2b08d56215be90213aae83c6ba0d9731f4bdc17c
fsy2001 <fsy2001@outlook.com>
2024-03-25 18:33:44 +0800

    refactor: remove deprecated API and views #15

--- Shared/Fudan Kit/Requests/FDWebVPNAPI.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct FDWebVPNAPI {
-    static func login() async throws {
-        let loginURL = URL(string: "https://webvpn.fudan.edu.cn/login?cas_login=true")!
-        _ = try await FDAuthAPI.auth(url: loginURL)
-    }
-}
