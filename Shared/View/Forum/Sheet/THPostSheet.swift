ed0f44bcdc35c5c09c47c5d7b21426f02220bbf5
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-03-27 14:32:43 +0800

    remove: tag suggestions

--- Shared/View/Forum/Sheet/THPostSheet.swift
+++ Shared/View/Forum/Sheet/THPostSheet.swift
@@ -7,14 +7,6 @@ struct THPostSheet: View {
     @AppStorage("post-content") private var content = ""
     @AppStorage("post-tag") private var tags: [String] = []
     
-    private func suggestTags() {
-        let input = content.stripToNLProcessableString()
-        guard let predictedTags = TagPredictor.shared?.suggest(input) else { return }
-        withAnimation {
-            tags = predictedTags
-        }
-    }
-    
     var body: some View {
         Sheet("New Post") {
             let hole = try await THRequests.createHole(
@@ -43,11 +35,6 @@ struct THPostSheet: View {
             }
             
             THTagEditor($tags, maxSize: 5)
-            Button {
-                suggestTags()
-            } label: {
-                Label("Suggest Tags", systemImage: "wand.and.rays")
-            }
             
             THContentEditor(content: $content)
         }
