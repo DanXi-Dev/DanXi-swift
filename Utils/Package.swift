6bd2ba439d62a1f4c2c56396f90a269cde3b9fbf
fsy2001 <fsy2001@outlook.com>
2024-03-28 16:40:33 +0800

    misc: add package Utils for utilities

---
+++ Utils/Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Utils",
+    products: [
+        .library(name: "Utils", targets: ["Utils"]),
+    ],
+    targets: [
+        .target(name: "Utils"),
+    ]
+)
