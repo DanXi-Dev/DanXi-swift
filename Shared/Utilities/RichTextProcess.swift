45213a6afbb16801fe1012005cb3c1b27a320045
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-04-04 15:48:17 +0800

    fix: #75

--- Shared/Utilities/RichTextProcess.swift
+++ Shared/Utilities/RichTextProcess.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 extension String {
-    /// Convert Treehole-formatted content to basic markdown, stripping images, references and LaTeX.
+    /// Convert Treehole-formatted content to basic markdown, stripping images and LaTeX.
     func stripToBasicMarkdown() -> String {
         let text = NSMutableString(string: self)
         
         _ = try? NSRegularExpression(pattern: #"\${1,2}.*?\${1,2}"#, options: .dotMatchesLineSeparators).replaceMatches(in: text, range: NSRange(location: 0, length: text.length), withTemplate: NSLocalizedString("formula_tag", comment: "Formula Tag"))
         _ = try? NSRegularExpression(pattern: #"!\[.*?\]\(.*?\)"#).replaceMatches(in: text, range: NSRange(location: 0, length: text.length), withTemplate: NSLocalizedString("image_tag", comment: "Image Tag"))
-        _ = try? NSRegularExpression(pattern: #"#{1,2}[0-9]+\s*"#).replaceMatches(in: text, range: NSRange(location: 0, length: text.length), withTemplate: "")
+//        _ = try? NSRegularExpression(pattern: #"#{1,2}[0-9]+\s*"#).replaceMatches(in: text, range: NSRange(location: 0, length: text.length), withTemplate: "")
         
         return String(text)
     }
     
-    /// Convert `String` to `AttributedString` using Markdown syntax, stripping images, references and LaTeX.
+    /// Convert `String` to `AttributedString` using Markdown syntax, stripping images and LaTeX.
     func inlineAttributed() -> AttributedString {
         let content = self.stripToBasicMarkdown()
         if let attributedString = try? AttributedString(markdown: content) {
