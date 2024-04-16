9591355802475d9404590b07a4ea6b9a21b6a0d6
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-04-16 10:50:34 +0800

    fix: #106

--- Shared/View/Forum/Page/THHomePage.swift
+++ Shared/View/Forum/Page/THHomePage.swift
@@ -8,7 +8,7 @@ struct THHomePage: View {
     @State private var openURL: URL? = nil
     
     var body: some View {
-        AsyncContentView(finished: forumModel.loaded) { _ in
+        AsyncContentView(finished: forumModel.loaded, refreshable: false) { _ in
             _ = try await appModel.loadUser() // load user first to prevent concurrently refresh token
             try await forumModel.loadAll()
         } content: {
