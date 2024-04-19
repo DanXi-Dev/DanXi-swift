c6506b4c39e6af1bf8db0fffe8079ba5c87f42bc
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-04-19 19:08:40 +0800

    change: notification page 🌴 ui

--- Shared/View/Forum/View/THNotificationView.swift
+++ Shared/View/Forum/View/THNotificationView.swift
@@ -4,89 +4,90 @@ struct THNotificationView: View {
     let message: THMessage
     
     var body: some View {
-        VStack(alignment: .leading) {
-            header
+        HStack(alignment: .top) {
+            message.icon
+                .padding(.trailing)
+                .padding(.vertical)
             
-            if message.code == .permission {
-                Text(message.description)
-            }
-            
-            if let floor = message.floor {
-                GroupBox {
-                    THSimpleFloor(floor: floor)
+            VStack(alignment: .leading) {
+                HStack(alignment: .top) {
+                    Text(message.title)
+                        .font(.headline)
+                    Spacer()
+                    Text(message.createTime.formatted())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
                 }
-            } else if let report = message.report {
-                GroupBox {
-                    THSimpleFloor(floor: report.floor)
+                
+                if message.code == .permission {
+                    Text(message.description)
                 }
-            } else if message.code == .mail {
-                GroupBox {
-                    HStack {
-                        Text(message.description)
-                        Spacer()
+                
+                if let floor = message.floor {
+                    GroupBox {
+                        THSimpleFloor(floor: floor)
+                    }
+                } else if let report = message.report {
+                    GroupBox {
+                        THSimpleFloor(floor: report.floor)
+                    }
+                } else if message.code == .mail {
+                    GroupBox {
+                        HStack {
+                            Text(message.description)
+                            Spacer()
+                        }
                     }
                 }
             }
-            
-            HStack {
-                Spacer()
-                Text(message.createTime.formatted())
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-            }
+        }
+    }
+}
+
+extension THMessage {
+    public var icon: Image {
+        switch self.code {
+        case .favorite:
+            Image(systemName: "bell")
+        case .reply:
+            Image(systemName: "arrowshape.turn.up.left")
+        case .mention:
+            Image(systemName: "quote.bubble")
+        case .modify:
+            Image(systemName: "bubble.left.and.exclamationmark.bubble.right")
+        case .permission:
+            Image(systemName: "bubble.left.and.exclamationmark.bubble.right")
+        case .report:
+            Image(systemName: "exclamationmark.circle")
+        case .reportDealt:
+            Image(systemName: "exclamationmark.circle")
+        case .mail:
+            Image(systemName: "envelope")
+        default:
+            Image(systemName: "questionmark.circle")
         }
     }
     
-    private var header: some View {
-        Group {
-            switch message.code {
-            case .favorite:
-                HStack {
-                    Image(systemName: "star")
-                    Text("New reply in favorites")
-                }
-            case .reply:
-                HStack {
-                    Image(systemName: "arrowshape.turn.up.left")
-                    Text("New reply")
-                }
-            case .mention:
-                HStack {
-                    Image(systemName: "quote.opening")
-                    Text("Mentioned")
-                }
-            case .modify:
-                HStack {
-                    Image(systemName: "exclamationmark.bubble")
-                    Text("Violation notice")
-                }
-            case .permission:
-                HStack {
-                    Image(systemName: "lock.trianglebadge.exclamationmark")
-                    Text("Ban notice")
-                }
-            case .report:
-                HStack {
-                    Image(systemName: "exclamationmark.circle")
-                    Text("New Report")
-                }
-            case .reportDealt:
-                HStack {
-                    Image(systemName: "checkmark.circle.trianglebadge.exclamationmark")
-                    Text("Report feedback")
-                }
-            case .mail:
-                HStack {
-                    Image(systemName: "envelope")
-                    Text("System mail")
-                }
-            default:
-                HStack {
-                    Image(systemName: "questionmark.circle")
-                }
-            }
+    public var title: LocalizedStringKey {
+        switch self.code {
+        case .favorite:
+            "New reply in favorites"
+        case .reply:
+            "New reply"
+        case .mention:
+            "Mentioned"
+        case .modify:
+            "Violation notice"
+        case .permission:
+            "Ban notice"
+        case .report:
+            "New Report"
+        case .reportDealt:
+            "Report feedback"
+        case .mail:
+            "System mail"
+        default:
+            "Unknown"
         }
-        .foregroundColor(.secondary)
-        .font(.callout)
     }
 }
