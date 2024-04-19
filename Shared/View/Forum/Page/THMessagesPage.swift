43a1775ee35c82611451a5b4a38b4c8616858faa
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-04-19 22:04:15 +0800

    feat: empty list reminder

--- Shared/View/Forum/Page/THMessagesPage.swift
+++ Shared/View/Forum/Page/THMessagesPage.swift
@@ -17,26 +17,32 @@ fileprivate struct NotificationContent: View {
     let messages: [THMessage]
     
     var body: some View {
-        THBackgroundList {
-            ForEach(messages) { message in
-                if let floor = message.floor {
-                    DetailLink(value: THHoleLoader(floorId: floor.id)) {
-                        THNotificationView(message: message)
+        Group {
+            if messages.isEmpty {
+                Text("No Content")
+            } else {
+                THBackgroundList {
+                    ForEach(messages) { message in
+                        if let floor = message.floor {
+                            DetailLink(value: THHoleLoader(floorId: floor.id)) {
+                                THNotificationView(message: message)
+                            }
+                        } else if let report = message.report {
+                            DetailLink(value: THHoleLoader(floorId: report.floor.id)) {
+                                THNotificationView(message: message)
+                            }
+                        } else {
+                            THNotificationView(message: message)
+                        }
                     }
-                } else if let report = message.report {
-                    DetailLink(value: THHoleLoader(floorId: report.floor.id)) {
-                        THNotificationView(message: message)
-                    }
-                } else {
-                    THNotificationView(message: message)
                 }
+                .listStyle(.inset)
+#if targetEnvironment(macCatalyst)
+                .listRowBackground(Color.clear)
+#endif
             }
         }
-        .listStyle(.inset)
         .navigationTitle("Notifications")
         .navigationBarTitleDisplayMode(.inline)
-#if targetEnvironment(macCatalyst)
-        .listRowBackground(Color.clear)
-#endif
     }
 }
