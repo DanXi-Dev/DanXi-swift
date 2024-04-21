e830884c1ea2fe1da6ab31c54488dbe503a978e3
fsy2001 <fsy2001@outlook.com>
2024-04-22 02:32:29 +0800

    revert: random color list

--- Shared/View/Support/RandomColor.swift
+++ Shared/View/Support/RandomColor.swift
@@ -8,12 +8,22 @@ func randomColor(_ name: String) -> Color {
         Color.red,
         Color.pink,
         Color.purple,
+        Color("danxi deep purple"),
+        Color("danxi indigo"),
         Color.blue,
+        Color("danxi light blue"),
         Color.cyan,
         Color.teal,
         Color.green,
+        Color("danxi light green"),
+        Color("danxi lime"),
+        Color("danxi yellow"),
+        Color("danxi amber"),
         Color.orange,
+        Color("danxi deep orange"),
         Color.brown,
+        Color("danxi blue grey"),
+        Color("danxi grey")
     ]
     
     if name.starts(with: "*") { // folding tags
