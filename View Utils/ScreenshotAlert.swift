33a04f81318f2da561c99257a483d95789009abc
fsy2001 <fsy2001@outlook.com>
2024-04-24 16:42:18 +0800

    fix: screenshot alert present unexpectedly in search page, close #128

--- View Utils/ScreenshotAlert.swift
+++ View Utils/ScreenshotAlert.swift
@@ -8,37 +8,44 @@ extension View {
 
 struct ScreenshotAlert<Content: View>: View {
     let content: Content
+    @StateObject private var model = ScreenshotAlertModel()
     private let screenshotPublisher = NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)
-    @State private var count = 0
     
     var body: some View {
         content
             .background {
-                ScreenshotAlertPresentor(count: count, title: String(localized: "Screenshot Detected"), message: String(localized: "Screenshot Alert Content"))
+                ScreenshotAlertPresentor()
+                    .environmentObject(model)
             }
             .onReceive(screenshotPublisher) { _ in
-                count += 1
+                model.presentAlert()
             }
     }
 }
 
-struct ScreenshotAlertPresentor: UIViewRepresentable {
-    let count: Int
-    let title: String
-    let message: String
+class ScreenshotAlertModel: ObservableObject {
+    weak var uiView: UIView? = nil
     
-    func makeUIView(context: Context) -> UIView {
-        UIView()
-    }
-    
-    func updateUIView(_ uiView: UIView, context: Context) {
-        guard count > 0 else { return }
-        
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    func presentAlert() {
+        let alertController = UIAlertController(title: String(localized: "Screenshot Detected"), message: String(localized: "Screenshot Alert Content"), preferredStyle: .alert)
         let defaultAction = UIAlertAction(title: String(localized: "OK"), style: .default) { _ in
             // do nothing
         }
         alertController.addAction(defaultAction)
-        uiView.window?.farthestPresentedViewController?.present(alertController, animated: true)
+        uiView?.window?.farthestPresentedViewController?.present(alertController, animated: true)
+    }
+}
+
+struct ScreenshotAlertPresentor: UIViewRepresentable {
+    @EnvironmentObject private var model: ScreenshotAlertModel
+    
+    func makeUIView(context: Context) -> UIView {
+        let uiView = UIView()
+        model.uiView = uiView
+        return uiView
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        // do nothing
     }
 }
