750a32a4944ab958d75ca0c87d3972b24310d98e
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-04-25 15:04:02 +0800

    change: date representation

--- Shared/Utilities/Date.swift
+++ Shared/Utilities/Date.swift
@@ -3,9 +3,9 @@ import Foundation
 extension Date {
     func autoFormatted() -> String {
         let now = Date.now
-        let oneWeekAgo = now.addingTimeInterval(-7 * 24 * 60 * 60)
+        let oneMonthAgo = now.addingTimeInterval(-30 * 24 * 60 * 60)
         
-        if self > oneWeekAgo {
+        if self > oneMonthAgo {
             return formatted(.relative(presentation: .named))
         } else {
             return formatted(date: .abbreviated, time: .omitted)
