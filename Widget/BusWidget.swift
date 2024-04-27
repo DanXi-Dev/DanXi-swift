b76070a42391520bf76bc16a70c8fd7bbfa304fe
Ivanfei <ivanfei2218@icloud.com>
2024-04-27 17:54:00 +0800

    do: remove intent from intentdifinition

--- Widget/BusWidget.swift
+++ Widget/BusWidget.swift
@@ -1,50 +1,71 @@
 import FudanKit
+import Intents
 import SwiftUI
 import WidgetKit
-import Intents
 
-struct BusWidgetProvier: IntentTimelineProvider {
+struct BusWidgetProvier: AppIntentTimelineProvider {
     typealias Entry = BusEntry
+    
     typealias Intent = BusScheduleIntent
     
     func placeholder(in context: Context) -> BusEntry {
         BusEntry()
     }
     
-    func getSnapshot(for configuation: BusScheduleIntent, in context: Context, completion: @escaping (BusEntry) -> Void) {
+    func snapshot(for configuration: BusScheduleIntent, in context: Context) async -> BusEntry {
         var entry = BusEntry()
+        let startPoint = configuration.startPoint
+        print(startPoint.rawValue)
         if !context.isPreview {
             entry.placeholder = true
         }
-        completion(entry)
+        return entry
     }
     
-    func getTimeline(for configuration: BusScheduleIntent, in context: Context, completion: @escaping (Timeline<BusEntry>) -> Void) {
-        Task {
-            do {
-                let (workdayRoutes, holidayRoutes) = try await BusStore.shared.getRefreshedRoutes()
-                let currentDate = Date()
-                let calendar = Calendar.current
-                
-                let entry = BusEntry(calendar.isDateInWeekend(currentDate) ? holidayRoutes : workdayRoutes)
-                
-                let date = Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!
-                let timeline = Timeline(entries: [entry], policy: .after(date))
-                completion(timeline)
-            } catch {
-                var entry = BusEntry()
-                entry.loadFailed = true
-                let date = Calendar.current.date(byAdding: .hour, value: 1, to: Date.now)!
-                let timeline = Timeline(entries: [entry], policy: .after(date))
-                completion(timeline)
+    func timeline(for configuration: BusScheduleIntent, in context: Context) async -> Timeline<BusEntry> {
+        do {
+            let (workdayRoutes, holidayRoutes) = try await BusStore.shared.getRefreshedRoutes()
+            let currentDate = Date()
+            let currentCalendar = Calendar.current
+            let startPoint = configuration.startPoint
+            let endPoint = configuration.endPoint
+            
+            let routes = currentCalendar.isDateInWeekend(currentDate) ? holidayRoutes : workdayRoutes
+            let filteredRoutes = routes.filter { $0.start == startPoint.rawValue && $0.end == endPoint.rawValue }.first
+            
+            for var schedule in filteredRoutes!.schedules {
+                let dateComponents = currentCalendar.dateComponents([.year, .month, .day], from: .now)
+                let busTimeComponents = currentCalendar.dateComponents([.hour, .minute, .second], from: schedule.time)
+    
+                var combinedComponents = DateComponents()
+                combinedComponents.year = dateComponents.year
+                combinedComponents.month = dateComponents.month
+                combinedComponents.day = dateComponents.day
+                combinedComponents.hour = busTimeComponents.hour
+                combinedComponents.minute = busTimeComponents.minute
+                combinedComponents.second = busTimeComponents.second
+
+                schedule.time = currentCalendar.date(from: combinedComponents)!
             }
+                
+            let entry = BusEntry(filteredRoutes)
+                
+            let date = Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!
+            let timeline = Timeline(entries: [entry], policy: .after(date))
+            return timeline
+        } catch {
+            var entry = BusEntry()
+            entry.loadFailed = true
+            let date = Calendar.current.date(byAdding: .hour, value: 1, to: Date.now)!
+            let timeline = Timeline(entries: [entry], policy: .after(date))
+            return timeline
         }
     }
 }
 
 public struct BusEntry: TimelineEntry {
     public let date: Date
-    public let routes: [FudanKit.Route]
+    public let route: FudanKit.Route?
     public var placeholder = false
     public var loadFailed = false
     
@@ -54,36 +75,23 @@ public struct BusEntry: TimelineEntry {
         let date1 = formatter.date(from: "2024-04-19 2:00")!
         let date2 = formatter.date(from: "2024-04-19 16:00")!
         date = Date()
-        routes = [
-            Route(start: "邯郸", end: "枫林", schedules: [
-                Schedule(id: 0, time: date1, start: "邯郸", end: "枫林", holiday: false, bidirectional: false),
-                Schedule(id: 0, time: date2, start: "邯郸", end: "枫林", holiday: false, bidirectional: false)])
-        ]
+        route = Route(start: "邯郸", end: "枫林", schedules: [
+            Schedule(id: 0, time: date1, start: "邯郸", end: "枫林", holiday: false, bidirectional: false),
+            Schedule(id: 0, time: date2, start: "邯郸", end: "枫林", holiday: false, bidirectional: false)])
     }
     
-    public init(_ routes: [FudanKit.Route]) {
+    public init(_ route: FudanKit.Route?) {
         date = Date()
-        self.routes = routes
+        self.route = route
     }
 }
 
-//struct BusScheduleSelector: Widget {
-//    let kind: String = "ecard.fudan.edu.cn"
-//    public var body: some WidgetConfiguration {
-//        IntentConfiguration(kind: kind, intent: BusScheduleIntent.self, provider: BusWidgetProvier()) { entry in
-//            BusWidgetView(entry: entry)
-//        }
-//        .configurationDisplayName("Bus")
-//        .description("Check school bus.")
-//        .supportedFamilies([.systemSmall])
-//    }
-//}
 
 public struct BusWidget: Widget {
     public init() {}
     
     public var body: some WidgetConfiguration {
-        IntentConfiguration(kind: "bus.fudan.edu.cn", intent: BusScheduleIntent.self, provider: BusWidgetProvier()) { entry in
+        AppIntentConfiguration(kind: "bus.fudan.edu.cn", intent: BusScheduleIntent.self, provider: BusWidgetProvier()) { entry in
             if #available(iOS 17.0, *) {
                 BusWidgetView(entry: entry)
                     .containerBackground(.fill.quinary, for: .widget)
@@ -119,22 +127,18 @@ struct BusWidgetView: View {
     }
     
     private var followingBus: some View {
-        let route = entry.routes.filter { route in
-            let directMatch = route.start == "邯郸" && route.end == "枫林"
-            return directMatch
-        }.first
         
-        guard let route = route else {
-            return AnyView(Text("No more bus"))
+        guard entry.route != nil else {
+            return AnyView(Text("No more bussss"))
         }
             
         let timeNow = Date.now
         
-        let schedules = route.schedules.filter { schedule in
+        let schedules = entry.route?.schedules.filter { schedule in
             schedule.time > timeNow
         }
         
-        if let schedule = schedules.first {
+        if let schedule = schedules?.first {
             // TODO: add 'if show nex day's bus' switch
             let formatter = DateFormatter()
             formatter.dateStyle = .none
@@ -153,7 +157,7 @@ struct BusWidgetView: View {
                 .fontWeight(.semibold)
                 .foregroundColor(.cyan)
                 
-                if let followingBus = schedules.dropFirst().first {
+                if let followingBus = schedules?.dropFirst().first {
                     Group {
                         Text("下一班 ") + Text(formatter.string(from: followingBus.time))
                     }
@@ -166,7 +170,7 @@ struct BusWidgetView: View {
                 }
             })
         } else {
-            return AnyView(Text("No more bus"))
+            return AnyView(Text("No more"))
         }
     }
     
