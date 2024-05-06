41529ce636fa4e17945a9cafd3d35c7ca90b8c7b
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-05-06 10:17:11 +0800

    change: remove unused weather function

--- Utils/CampusWeather.swift
+++ Utils/CampusWeather.swift
@@ -7,12 +7,6 @@
 
 /*
 import WeatherKit
-import CoreLocation
-
-public let HandanCampusLocation = CLLocation(latitude: 31.301911, longitude: 121.510771)
-public let FenglinCampusLocation = CLLocation(latitude: 31.203908, longitude: 121.458458)
-public let JiangwanCampusLocation = CLLocation(latitude: 31.34272, longitude: 121.513724)
-public let ZhangjiangCampusLocation = CLLocation(latitude: 31.196204, longitude: 121.60448)
 
 public class CampusWeather {
     public static let shared = CampusWeather()
