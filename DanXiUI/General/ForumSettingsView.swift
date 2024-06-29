f00c47e3c2ef71886a7de6a56b7c1f6637004c62
Ivanfei <ivanfei2218@icloud.com>
2024-06-29 21:04:24 +0800

    fix: 🌴 icon

--- DanXiUI/General/ForumSettingsView.swift
+++ DanXiUI/General/ForumSettingsView.swift
@@ -37,7 +37,7 @@ public enum ForumSettingsSection: Identifiable, Hashable, CaseIterable {
         case .blockedContent:
             Label(String(localized: "Blocked Content", bundle: .module), systemImage: "hand.raised.app")
         case .advancedSettings:
-            Label(String(localized: "Advanced Settings", bundle: .module), systemImage: "gearshape.circle.fill")
+            Label(String(localized: "Advanced Settings", bundle: .module), systemImage: "gearshape.circle")
         }
     }
     
