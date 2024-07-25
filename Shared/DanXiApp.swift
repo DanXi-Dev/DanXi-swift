02aed1140f614e9328ac2ce00cb8777e33b36c24
fsy2001 <fsy2001@outlook.com>
2024-07-25 15:37:03 +0800

    fix: app loses state between entering background, close #217

--- Shared/DanXiApp.swift
+++ Shared/DanXiApp.swift
@@ -18,5 +18,3 @@ struct DanXiApp: App {
         }
     }
 }
-
-
---
+++ View Utils/Navigation/WideScreenReader.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+/**
+ A view that switch between compact and regular view while preventing app to lose state.
+ 
+ When the app enters background, the `horizontalSizeClass` will change between `.regular` and `.compact`.
+ If the view directly depend on `horizontalSizeClass`, this change causes the view to recompute and lose state.
+ This wrapper is a workaround.
+ */
+public struct WideScreenReader<Wide: View, Narrow: View>: View {
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @Environment(\.scenePhase) private var scenePhase
+    
+    @StateObject private var model = WideScreenReaderModel()
+    
+    private var actualHorizontalSizeClass: UserInterfaceSizeClass? {
+        if scenePhase == .active {
+            model.storedHorizontalSizeClass = horizontalSizeClass
+            return horizontalSizeClass
+        } else {
+            return model.storedHorizontalSizeClass
+        }
+    }
+    
+    private let wide: () -> Wide
+    private let narrow: () -> Narrow
+    
+    public init(wide: @escaping () -> Wide, narrow: @escaping () -> Narrow) {
+        self.wide = wide
+        self.narrow = narrow
+    }
+    
+    public var body: some View {
+        if actualHorizontalSizeClass == .compact {
+            narrow()
+        } else {
+            wide()
+        }
+    }
+}
+
+private class WideScreenReaderModel: ObservableObject {
+    var storedHorizontalSizeClass: UserInterfaceSizeClass? = nil
+}
