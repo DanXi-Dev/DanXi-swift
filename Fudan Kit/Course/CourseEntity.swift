0002aa54a1d6ca29a2bbb5480ba9fea9ada35e68
Ivanfei <67986259+ivanfei-1@users.noreply.github.com>
2024-09-08 23:15:54 +0800

    fix: wrong course time, close #266 (#269)
    * fix: wrong course time when initiating using string fails due to region settings, close #266
    
    * refactor: reformat CourseEntity.swift
    
    * fix: exporting to calendar cause the courses to have wrong timezones

--- Fudan Kit/Course/CourseEntity.swift
+++ Fudan Kit/Course/CourseEntity.swift
@@ -9,7 +9,7 @@ public struct Semester: Codable {
     public enum SemesterType: Int, Codable, Hashable {
         case first = 0, winter, second, summer
     }
-    
+
     let year: Int
     let type: SemesterType
     public let semesterId: Int
@@ -39,21 +39,21 @@ extension Semester: Comparable {
         if lhs.year != rhs.year {
             return lhs.year < rhs.year
         }
-        
+
         return lhs.type.rawValue < rhs.type.rawValue
     }
 }
 
-extension Semester {
+public extension Semester {
     /// Formatted name of the semester
-    public var name: String {
+    var name: String {
         let semesterName = switch type {
         case .first: "第一学期"
         case .winter: "寒假学期"
         case .second: "第二学期"
         case .summer: "暑假学期"
         }
-        
+
         return "\(year)-\(year + 1)年\(semesterName)"
     }
 }
@@ -65,10 +65,10 @@ public struct Course: Identifiable, Codable, Hashable {
     public let weekday: Int
     public let start, end: Int
     public let onWeeks: [Int]
-    
+
     public func openOn(_ week: Int) -> Bool {
-            onWeeks.contains(week)
-        }
+        onWeeks.contains(week)
+    }
 }
 
 /// Time slot that courses are held.
@@ -76,52 +76,39 @@ public struct Course: Identifiable, Codable, Hashable {
 /// All possible time slots have been created.
 /// Do not create your own time slot.
 /// Retrieve time slot by calling ``getItem(_:)``
-public struct ClassTimeSlot: Identifiable{
+public struct ClassTimeSlot: Identifiable {
     public let id: Int
     public let start, end: Date
 }
 
-extension ClassTimeSlot {
-    init(_ id: Int, _ start: String, _ end: String) {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm"
+public extension ClassTimeSlot {
+    internal init(_ id: Int, _ start: DateComponents, _ end: DateComponents) {
         self.id = id
-        
+
         /*
-        // start and end are specified in below, it cannot in incorrect format
-        // it's safe to use force unwrap
-        self.start = formatter.date(from: start)!
-        self.end = formatter.date(from: end)!
-         */
-        
-        // The original version causes crash, and I don't know why.
-        // This is a temporary fix.
-        if let start = formatter.date(from: start), let end = formatter.date(from: end) {
-            self.start = start
-            self.end = end
-        } else {
-            // instead of crashing the app, this will set incorrect time data
-            self.start = Date()
-            self.end = Date()
-        }
+         // start and end are specified in below, it cannot in incorrect format
+         // it's safe to use force unwrap
+          */
+        self.start = start.date!
+        self.end = end.date!
     }
-    
-    public static let list = [ClassTimeSlot(1, "08:00", "08:45"),
-                              ClassTimeSlot(2, "08:55", "09:40"),
-                              ClassTimeSlot(3, "09:55", "10:40"),
-                              ClassTimeSlot(4, "10:50", "11:35"),
-                              ClassTimeSlot(5, "11:45", "12:30"),
-                              ClassTimeSlot(6, "13:30", "14:15"),
-                              ClassTimeSlot(7, "14:25", "15:10"),
-                              ClassTimeSlot(8, "15:25", "16:10"),
-                              ClassTimeSlot(9, "16:20", "17:05"),
-                              ClassTimeSlot(10, "17:15", "18:00"),
-                              ClassTimeSlot(11, "18:30", "19:15"),
-                              ClassTimeSlot(12, "19:25", "20:10"),
-                              ClassTimeSlot(13, "20:20", "21:05"),
-                              ClassTimeSlot(14, "21:15", "22:00")]
-    
-    public static func getItem(_ i: Int) -> ClassTimeSlot {
+
+    static let list = [ClassTimeSlot(1, DateComponents(calendar: Calendar.current, hour: 8, minute: 0), DateComponents(calendar: Calendar.current, hour: 8, minute: 45)),
+                       ClassTimeSlot(2, DateComponents(calendar: Calendar.current, hour: 8, minute: 55), DateComponents(calendar: Calendar.current, hour: 9, minute: 40)),
+                       ClassTimeSlot(3, DateComponents(calendar: Calendar.current, hour: 9, minute: 55), DateComponents(calendar: Calendar.current, hour: 10, minute: 40)),
+                       ClassTimeSlot(4, DateComponents(calendar: Calendar.current, hour: 10, minute: 50), DateComponents(calendar: Calendar.current, hour: 11, minute: 35)),
+                       ClassTimeSlot(5, DateComponents(calendar: Calendar.current, hour: 11, minute: 45), DateComponents(calendar: Calendar.current, hour: 12, minute: 30)),
+                       ClassTimeSlot(6, DateComponents(calendar: Calendar.current, hour: 13, minute: 30), DateComponents(calendar: Calendar.current, hour: 14, minute: 15)),
+                       ClassTimeSlot(7, DateComponents(calendar: Calendar.current, hour: 14, minute: 25), DateComponents(calendar: Calendar.current, hour: 15, minute: 10)),
+                       ClassTimeSlot(8, DateComponents(calendar: Calendar.current, hour: 15, minute: 25), DateComponents(calendar: Calendar.current, hour: 16, minute: 10)),
+                       ClassTimeSlot(9, DateComponents(calendar: Calendar.current, hour: 16, minute: 20), DateComponents(calendar: Calendar.current, hour: 17, minute: 5)),
+                       ClassTimeSlot(10, DateComponents(calendar: Calendar.current, hour: 17, minute: 15), DateComponents(calendar: Calendar.current, hour: 18, minute: 0)),
+                       ClassTimeSlot(11, DateComponents(calendar: Calendar.current, hour: 18, minute: 30), DateComponents(calendar: Calendar.current, hour: 19, minute: 15)),
+                       ClassTimeSlot(12, DateComponents(calendar: Calendar.current, hour: 19, minute: 25), DateComponents(calendar: Calendar.current, hour: 20, minute: 10)),
+                       ClassTimeSlot(13, DateComponents(calendar: Calendar.current, hour: 20, minute: 20), DateComponents(calendar: Calendar.current, hour: 21, minute: 5)),
+                       ClassTimeSlot(14, DateComponents(calendar: Calendar.current, hour: 21, minute: 15), DateComponents(calendar: Calendar.current, hour: 22, minute: 0))]
+
+    static func getItem(_ i: Int) -> ClassTimeSlot {
         return list[i - 1]
     }
 }
@@ -160,7 +147,7 @@ public struct Rank: Identifiable, Codable {
     public let grade, major, department: String
     public let gradePoint, credit: Double
     public let rank: Int
-    
+
     public var isMe: Bool {
         !name.contains("*")
     }
