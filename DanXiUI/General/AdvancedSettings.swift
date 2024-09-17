f5218a440ad0b89f15c4f09181c798b8e591dc5e
fsy2001 <fsy2001@outlook.com>
2024-09-18 00:20:26 +0800

    feat: include control for preview feature (innovation bank)

--- DanXiUI/General/AdvancedSettings.swift
+++ DanXiUI/General/AdvancedSettings.swift
@@ -28,6 +28,16 @@ struct AdvancedSettings: View {
             } footer: {
                 Text("Use Fudan University WebVPN service to access Forum and DanKe from off-campus.\n\nWhen this is off, most features will only be available within the campus intranet. Campus Assistant automatically adjusts this setting based on your network condition. You do not have to change this in most occasions.", bundle: .module)
             }
+            
+            Section {
+                Picker(String(localized: "Preview Features", bundle: .module), selection: $settings.previewFeatureSetting) {
+                    Text("Show", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.show)
+                    Text("Focus", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.focus)
+                    Text("Hide", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.hide)
+                }
+            } footer: {
+                Text("Control the appearance of preview features, currently including innovation bank", bundle: .module)
+            }
         }
         .navigationTitle(String(localized: "Advanced Settings", bundle: .module))
         .navigationBarTitleDisplayMode(.inline)
