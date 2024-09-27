d1fb211c6c3812928d70f5931456a862fa9965ce
fsy2001 <fsy2001@outlook.com>
2024-09-27 18:53:24 +0800

    feat: forward and backward button

--- DanXiUI/Innovation/InnovationHome.swift
+++ DanXiUI/Innovation/InnovationHome.swift
@@ -4,29 +4,60 @@ import WebKit
 
 public struct InnovationHomePage: View {
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @StateObject private var model = WebViewModel()
     
     public init() { }
     
     public var body: some View {
-        if horizontalSizeClass == .compact {
-            GeometryReader { proxy in
-                WebView(frame: proxy.frame(in: .local))
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+        GeometryReader { proxy in
+            WebView(frame: proxy.frame(in: .local))
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .environmentObject(model)
+        }
+        .ignoresSafeArea(.all)
+        .toolbar {
+            Button {
+                model.goBack()
+            } label: {
+                Image(systemName: "chevron.left")
             }
-            .toolbar(.hidden, for: .navigationBar)
-        } else {
-            GeometryReader { proxy in
-                WebView(frame: proxy.frame(in: .local))
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .disabled(!model.canGoBack)
+            
+            Button {
+                model.goForward()
+            } label: {
+                Image(systemName: "chevron.right")
             }
-            .ignoresSafeArea(.all)
-            .navigationBarHidden(true)
+            .disabled(!model.canGoForward)
         }
     }
 }
 
+private class WebViewModel: ObservableObject {
+    @Published var canGoBack = false
+    @Published var canGoForward = false
+    
+    weak var webview: WKWebView? = nil
+    
+    func goBack() {
+        webview?.goBack()
+    }
+    
+    func goForward() {
+        webview?.goForward()
+    }
+}
+
 private struct WebView: UIViewRepresentable {
+    @EnvironmentObject private var model: WebViewModel
+    
+    typealias Coordinator = WebViewModel
+    
     let frame: CGRect
+    
+    func makeCoordinator() -> WebViewModel {
+        model
+    }
     
     func makeUIView(context: Context) -> WKWebView {
         let config = WKWebViewConfiguration()
@@ -58,11 +89,16 @@ private struct WebView: UIViewRepresentable {
         let webView = WKWebView(frame: frame, configuration: config)
         let url = URL(string: "https://danta.fudan.edu.cn/lobby/1")!
         let request = URLRequest(url: url)
+        webView.allowsBackForwardNavigationGestures = true
         webView.load(request)
+        context.coordinator.webview = webView
         return webView
     }
     
     func updateUIView(_ webView: WKWebView, context: Context) {
-        // do nothing
+        DispatchQueue.main.async {
+            context.coordinator.canGoBack = webView.canGoBack
+            context.coordinator.canGoForward = webView.canGoForward
+        }
     }
 }
