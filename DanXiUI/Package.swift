61275f5774d979cac41d33e770f52fc6a72a539c
fsy2001 <fsy2001@outlook.com>
2024-10-16 23:31:16 +0800

    feat: community preview framework

--- DanXiUI/Package.swift
+++ DanXiUI/Package.swift
@@ -11,18 +11,19 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", from: "0.2.2"),
+        .package(url: "https://github.com/stleamist/BetterSafariView.git", .upToNextMajor(from: "2.4.2")),
         .package(name: "DanXiKit", path: "../DanXi Kit"),
         .package(name: "ViewUtils", path: "../View Utils"),
         .package(name: "Utils", path: "../Utils"),
     ],
     targets: [
-        .regexTarget(name: "DanXiUI", dependencies: ["SwiftUIX", "DanXiKit", "ViewUtils", "Utils"], path: "."),
+        .regexTarget(name: "DanXiUI", dependencies: ["SwiftUIX", "BetterSafariView", "DanXiKit", "ViewUtils", "Utils"], path: ".", resources: [.copy("Preview")]),
     ]
 )
 
 extension Target {
-    static func regexTarget(name: String, dependencies: [Target.Dependency], path: String) -> Target {
-        let target = target(name: name, dependencies: dependencies, path: path)
+    static func regexTarget(name: String, dependencies: [Target.Dependency], path: String, resources: [Resource]) -> Target {
+        let target = target(name: name, dependencies: dependencies, path: path, resources: resources)
         if target.swiftSettings == nil {
             target.swiftSettings = []
         }
