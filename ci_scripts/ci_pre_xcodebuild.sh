f550b1ffbc89303022e94b8d743d0cdc1268112a
fsy2001 <fsy2001@outlook.com>
2024-10-21 21:43:46 +0800

    ci: remove preview data in release build

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+cd $CI_PRIMARY_REPOSITORY_PATH/FudanUI || exit 1
+rm -rf Preview/*
+
+cd $CI_PRIMARY_REPOSITORY_PATH/DanXiUI || exit 1
+rm -rf Preview/*
+
 cd $CI_PRIMARY_REPOSITORY_PATH/Utils || exit 1
 printf "{\"authTestURL\":\"%s\",\"forumTestURL\":\"%s\",\"curriculumTestURL\":\"%s\"}" "$AUTH_TEST_URL" "$FORUM_TEST_URL" "$CURRICULUM_TEST_URL" > demo.json
