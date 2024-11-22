417f8c3e23471958a961f975abf94a7b0f204f73
fsy2001 <fsy2001@outlook.com>
2024-11-23 01:41:51 +0800

    misc: hide innovation bank until release

--- DanXiUI/General/AdvancedSettings.swift
+++ DanXiUI/General/AdvancedSettings.swift
@@ -29,17 +29,23 @@ struct AdvancedSettings: View {
                 Text("Use Fudan University WebVPN service to access Forum and DanKe from off-campus.\n\nWhen this is off, most features will only be available within the campus intranet. Campus Assistant automatically adjusts this setting based on your network condition. You do not have to change this in most occasions.", bundle: .module)
             }
             
-            Section {
-                Picker(String(localized: "Preview Features", bundle: .module), selection: $settings.previewFeatureSetting) {
-                    Text("Show", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.show)
-                    Text("Focus", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.focus)
-                    Text("Hide", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.hide)
-                }
-            } footer: {
-                Text("Control the appearance of preview features, currently including innovation bank", bundle: .module)
-            }
+            // previewFeatureSetting
+            
         }
         .navigationTitle(String(localized: "Advanced Settings", bundle: .module))
         .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    private var previewFeatureSetting: some View {
+        // TODO: This should be added once the innovation bank is released
+        Section {
+            Picker(String(localized: "Preview Features", bundle: .module), selection: $settings.previewFeatureSetting) {
+                Text("Show", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.show)
+                Text("Focus", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.focus)
+                Text("Hide", bundle: .module).tag(ForumSettings.PreviewFeatureSetting.hide)
+            }
+        } footer: {
+            Text("Control the appearance of preview features, currently including innovation bank", bundle: .module)
+        }
     }
 }
