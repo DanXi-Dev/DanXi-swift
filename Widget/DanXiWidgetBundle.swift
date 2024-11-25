49656c92113fc5400ddf296e123df3b664ff6323
singularity <12184989+singularity-s0@users.noreply.github.com>
2024-11-25 21:22:44 +0800

    fix: typo

--- Widget/DanXiWidgetBundle.swift
+++ Widget/DanXiWidgetBundle.swift
@@ -3,7 +3,7 @@ import SwiftUI
 import WidgetKit
 
 @main
-struct DanXiWigdetBundle: WidgetBundle {
+struct DanXiWidgetBundle: WidgetBundle {
     var body: some Widget {
         WalletWidget()
         if #available(iOS 17.0, *) {
@@ -12,12 +12,12 @@ struct DanXiWigdetBundle: WidgetBundle {
     }
 }
 
-@available(iOS 17, *)
-#Preview("Wallet", as: .systemSmall) {
-    WalletWidget()
-} timeline: {
-    WalletEntry()
-}
+//@available(iOS 17, *)
+//#Preview("Wallet", as: .systemSmall) {
+//    WalletWidget()
+//} timeline: {
+//    WalletEntry()
+//}
 
 @available(iOS 17, *)
 #Preview("Bus", as: .systemSmall) {
