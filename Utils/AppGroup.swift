f802a70e181ff098f267a3343dcdf89b5ef2c045
fsy2001 <fsy2001@outlook.com>
2024-11-28 01:28:29 +0800

    chore: modify Util package for watchOS

--- Utils/AppGroup.swift
+++ Utils/AppGroup.swift
@@ -5,6 +5,7 @@
 //  Created by Kavin Zhao on 2024-05-02.
 //
 
+#if !os(watchOS)
 import Disk
 
 public let AppGroupName = "group.com.fduhole.danxi"
@@ -12,3 +13,4 @@ public let AppGroupName = "group.com.fduhole.danxi"
 public extension Disk.Directory {
     static let appGroup = Disk.Directory.sharedContainer(appGroupName: AppGroupName)
 }
+#endif
