5fb6856d6ab71c9d4d8a1a6fb0853cb7202d7b79
Ivanfei <ivanfei2218@icloud.com>
2025-01-07 01:56:47 +0800

    feat: clear cache buttons

--- DanXiUI/General/AdvancedSettings.swift
+++ DanXiUI/General/AdvancedSettings.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 import DanXiKit
 import FudanKit
+import Disk
+import KeychainAccess
 
 struct AdvancedSettings: View {
     @ObservedObject private var settings = ForumSettings.shared
     @ObservedObject private var campusModel = CampusModel.shared
     @ObservedObject private var proxySettings = ProxySettings.shared
+    @State private var showClearAllCacheWarning = false
     
     var body: some View {
         Form {
@@ -31,6 +34,55 @@ struct AdvancedSettings: View {
             
             // previewFeatureSetting
             
+            Section {
+                Button {
+                    Task.detached {
+                        try Disk.remove("cachedimages/", from: .caches)
+                    }
+                } label: {
+                    Text("Clear Image Cache", bundle: .module)
+                }
+            } footer: {
+                Text("Clear image cache to free up disk space.", bundle: .module)
+            }
+            
+            Section {
+                Button(role: .destructive) {
+                    showClearAllCacheWarning = true
+                } label: {
+                    Text("Clear Entire Cache", bundle: .module)
+                }
+            } footer: {
+                Text("Clear entire cache will clear all data. This cannot be undone.", bundle: .module)
+            }
+            
+        }
+        .alert(String(localized: "Confirm Clear Entire Cache", bundle: .module), isPresented: $showClearAllCacheWarning) {
+            Button(role: .destructive) {
+                Task.detached {
+                    try? Disk.remove("fduhole/", from: .appGroup)
+                    try? Disk.remove("fdutools/", from: .appGroup)
+                    try? Disk.remove("cachedimages/", from: .caches)
+                    let communityKeychain = Keychain(service: "com.fduhole.danxi")
+                    communityKeychain[data: "token"] = nil
+                    let campusKeychain = Keychain(service: "com.fduhole.fdutools", accessGroup: "group.com.fduhole.danxi")
+                    campusKeychain["username"] = nil
+                    campusKeychain["password"] = nil
+                    campusKeychain["campus-student-type"] = nil
+                    
+                    let defaults = UserDefaults.standard
+                    let dictionary = defaults.dictionaryRepresentation()
+                    dictionary.keys.forEach { key in
+                        defaults.removeObject(forKey: key)
+                    }
+                    
+                    exit(0) // close app to clear memory
+                }
+            } label: {
+                Text("Clear Entire Cache", bundle: .module)
+            }
+        } message: {
+            Text("Clear entire cache. The app will restart after this operation.", bundle: .module)
         }
         .navigationTitle(String(localized: "Advanced Settings", bundle: .module))
         .navigationBarTitleDisplayMode(.inline)
