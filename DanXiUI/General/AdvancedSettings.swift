62bea13a0017b5f871e32b60e55c4dec457c548f
Ivanfei <67986259+ivanfei-1@users.noreply.github.com>
2025-01-13 17:55:33 +0800

    feat: clear cache button, close #364 (#366)
    * feat: clear cache buttons
    
    * remove unplanned todo
    
    * add: localizable for clearing cache buttons
    
    * enhancement: make clear cache button description clearer
    
    * enhancement: button description

--- DanXiUI/General/AdvancedSettings.swift
+++ DanXiUI/General/AdvancedSettings.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 import DanXiKit
 import FudanKit
+import Disk
+import KeychainAccess
 
 struct AdvancedSettings: View {
     @ObservedObject private var settings = ForumSettings.shared
     @ObservedObject private var campusModel = CampusModel.shared
     @ObservedObject private var proxySettings = ProxySettings.shared
+    @State private var showClearAllCacheWarning = false
     
     var body: some View {
         Form {
@@ -31,6 +34,55 @@ struct AdvancedSettings: View {
             
             // previewFeatureSetting
             
+            Section {
+                Button {
+                    Task.detached {
+                        try Disk.remove("cachedimages/", from: .caches)
+                    }
+                } label: {
+                    Text("Clear Image Cache", bundle: .module)
+                }
+            } footer: {
+                Text("Clear image cache to free up disk space.", bundle: .module)
+            }
+            
+            Section {
+                Button(role: .destructive) {
+                    showClearAllCacheWarning = true
+                } label: {
+                    Text("Clear All Data", bundle: .module)
+                }
+            } footer: {
+                Text("Clear all local data, including login accounts, search history, image cache, calendar and course cache, etc. It is generally used to fix issues such as freezing or failure to load content. This operation cannot be undone, so please use it under the guidance of the development team.", bundle: .module)
+            }
+            
+        }
+        .alert(String(localized: "Confirm Clearing All Data", bundle: .module), isPresented: $showClearAllCacheWarning) {
+            Button(role: .destructive) {
+                Task.detached {
+                    try? Disk.remove("fduhole/", from: .appGroup)
+                    try? Disk.remove("fdutools/", from: .appGroup)
+                    try? Disk.remove("cachedimages/", from: .caches)
+                    let communityKeychain = Keychain(service: "com.fduhole.danxi")
+                    communityKeychain[data: "token"] = nil
+                    let campusKeychain = Keychain(service: "com.fduhole.fdutools", accessGroup: "group.com.fduhole.danxi")
+                    campusKeychain["username"] = nil
+                    campusKeychain["password"] = nil
+                    campusKeychain["campus-student-type"] = nil
+                    
+                    let defaults = UserDefaults.standard
+                    let dictionary = defaults.dictionaryRepresentation()
+                    dictionary.keys.forEach { key in
+                        defaults.removeObject(forKey: key)
+                    }
+                    
+                    exit(0) // close app to clear memory
+                }
+            } label: {
+                Text("Clear All Data", bundle: .module)
+            }
+        } message: {
+            Text("Clear all data. The app will restart after this operation.", bundle: .module)
         }
         .navigationTitle(String(localized: "Advanced Settings", bundle: .module))
         .navigationBarTitleDisplayMode(.inline)
