ad2d83528724f911a7e8565ce19c871fe31fece6
fsy2001 <fsy2001@outlook.com>
2025-01-20 17:40:34 +0800

    fix: cannot access classroom info due to webvpn update

--- Fudan Kit/Classroom/ClassroomStore.swift
+++ Fudan Kit/Classroom/ClassroomStore.swift
@@ -5,17 +5,40 @@ public actor ClassroomStore {
     
     var classroomsCache: [Building: [Classroom]] = [:]
     
+    private var lastLoginDate: Date? = nil
+    
+    private var shouldLogin: Bool {
+        guard let lastLoginDate else { return true }
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 2
+        
+        let calendar = Calendar.current
+        guard let addedDate = calendar.date(byAdding: dateComponents, to: lastLoginDate) else { return true }
+        
+        return addedDate < Date.now
+    }
+    
+    private func authenticate() async throws {
+        if shouldLogin {
+            try await authenticateWebVPN()
+            lastLoginDate = Date()
+        }
+    }
+    
     public func getCachedClassroom(building: Building) async throws -> [Classroom] {
         if let classrooms = classroomsCache[building] {
             return classrooms
         }
         
+        try await authenticate()
         let classrooms = try await ClassroomAPI.getClassrooms(building: building)
         classroomsCache[building] = classrooms
         return classrooms
     }
     
     public func getRefreshedClassroom(building: Building) async throws -> [Classroom] {
+        try await authenticate()
         let classrooms = try await ClassroomAPI.getClassrooms(building: building)
         classroomsCache[building] = classrooms
         return classrooms
