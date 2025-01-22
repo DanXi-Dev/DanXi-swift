ed4e44ff9e15b48c0e97a98f667bcf5f8cf63a87
Ivanfei <ivanfei2218@icloud.com>
2025-01-23 01:03:02 +0800

    change: adjust electricity widget ui

--- FudanUI/Widgets/ElectricityWidget.swift
+++ FudanUI/Widgets/ElectricityWidget.swift
@@ -5,9 +5,9 @@
 //  Created by 袁新宇 on 2025/1/20.
 
 #if !os(watchOS)
-import WidgetKit
-import SwiftUI
 import FudanKit
+import SwiftUI
+import WidgetKit
 
 struct ElectricityWidgetProvier: TimelineProvider {
     func placeholder(in context: Context) -> ElectricityEntry {
@@ -22,7 +22,7 @@ struct ElectricityWidgetProvier: TimelineProvider {
         completion(entry)
     }
     
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
         Task {
             do {
                 let usage = try await ElectricityStore.shared.getCachedElectricityUsage()
@@ -92,9 +92,9 @@ public struct ElectricityEntry: TimelineEntry {
         self.average = average
         let ratio = electricityAvailable / average
         
-        self.warnLevel = switch ratio {
+        warnLevel = switch ratio {
         case ..<0.5: .critical
-        case 0.5..<1.8: .low
+        case 0.5 ..< 1.8: .low
         case 1.8...: .full
         default: .full
         }
@@ -103,7 +103,7 @@ public struct ElectricityEntry: TimelineEntry {
 
 @available(iOS 17.0, *)
 public struct ElectricityWidget: Widget {
-    public init() { }
+    public init() {}
     
     public var body: some WidgetConfiguration {
         StaticConfiguration(kind: "electricity.fudan.edu.cn", provider: ElectricityWidgetProvier()) { entry in
@@ -147,63 +147,47 @@ struct ElectricityWidgetView: View {
                 Label(String(localized: "Dorm Electricity", bundle: .module), systemImage: "bolt.fill")
                     .bold()
                     .font(.caption)
+                    .fontDesign(.rounded)
                     .foregroundColor(.green)
                 Spacer()
             }
-            .padding(.bottom, 18)
+            .padding(.bottom, 1)
             
-            switch entry.warnLevel {
-            case .full:
-                HStack{
-                    Image(systemName: "battery.100")
-                        .foregroundColor(widgetColor)
-                    Text(String(localized: "Sufficient charge", bundle: .module))
-                        .bold()
-                        .font(.system(size: 22, design: .rounded))
-                        .privacySensitive()
-                        .foregroundColor(widgetColor)
-                }
-            case .low:
-                HStack{
-                    Image(systemName: "battery.50percent")
-                        .foregroundColor(widgetColor)
-                    Text(String(localized: "Battery tension", bundle: .module))
-                        .bold()
-                        .font(.system(size: 22, design: .rounded))
-                        .privacySensitive()
-                        .foregroundColor(widgetColor)
-                }
-            case .critical:
-                HStack{
-                    Image(systemName: "battery.25percent")
-                        .foregroundColor(widgetColor)
-                    Text(String(localized: "Power cut imminent", bundle: .module))
-                        .bold()
-                        .font(.system(size: 22, design: .rounded))
-                        .privacySensitive()
-                        .foregroundColor(widgetColor)
-                }
-            }
+            Text("Daily usage: \(String(format: "%.2f", entry.average)) kWh", bundle: .module)
+                .foregroundColor(.secondary)
+                .font(.caption2)
             
             Spacer()
             
-            VStack(alignment: .leading, spacing: 0) {
-                Text(String(localized: "Remaining battery capacity", bundle: .module))
-                    .font(.caption2)
+            Text("Remains", bundle: .module)
+                .foregroundColor(.secondary)
+                .font(.caption2)
+            HStack(alignment: .firstTextBaseline, spacing: 0) {
+                Text(String(format: "%.2f", entry.electricityAvailable))
+                    .bold()
+                    .font(.title2)
+                    .fontDesign(.rounded)
+                
+                Text(verbatim: " kWh")
                     .foregroundColor(.secondary)
-                HStack(alignment: .firstTextBaseline, spacing: 0) {
-                    Text(String(format: "%.2f", entry.electricityAvailable))
-                        .bold()
-                        .font(.system(size: 20, design: .rounded))
-                        .privacySensitive()
-                    
-                    Text(verbatim: " ")
-                    Text(verbatim: "kWh")
-                        .foregroundColor(.secondary)
-                        .bold()
-                        .font(.caption2)
+                    .bold()
+                    .font(.caption2)
+            }
+            
+            HStack {
+                switch entry.warnLevel {
+                case .full:
+                    Label(String(localized: "Sufficient", bundle: .module), systemImage: "battery.100")
+                        .foregroundColor(.green)
+                case .low:
+                    Label(String(localized: "Low", bundle: .module), systemImage: "battery.50percent")
+                        .foregroundColor(.orange)
+                case .critical:
+                    Label(String(localized: "Critical", bundle: .module), systemImage: "battery.25percent")
+                        .foregroundColor(.red)
                 }
             }
+            .font(.caption)
         }
     }
 }
