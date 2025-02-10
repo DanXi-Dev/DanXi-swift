0c561ea502faab7f22fe9fa579c750977ff9f1ea
fsy2001 <fsy2001@outlook.com>
2025-02-10 21:34:22 +0800

    chore: remove ununsed code

--- DanXiUI/Utils/CustomMarkdown.swift
+++ DanXiUI/Utils/CustomMarkdown.swift
@@ -93,16 +93,4 @@ extension String {
         }
         return AttributedString(content)
     }
-    
-    /// Replace elements like formula and images to tags for ML to process.
-    func stripToNLProcessableString() -> String {
-        let text = NSMutableString(string: self)
-        
-        _ = try? NSRegularExpression(pattern: #"\${1,2}.*?\${1,2}"#, options: .dotMatchesLineSeparators).replaceMatches(in: text, range: NSRange(location: 0, length: text.length), withTemplate: "[Formula]")
-        _ = try? NSRegularExpression(pattern: #"!\[.*?\]\(.*?\)"#).replaceMatches(in: text, range: NSRange(location: 0, length: text.length), withTemplate: "[Image]")
-        _ = try? NSRegularExpression(pattern: #"\[.*?\]\(.*?\)"#).replaceMatches(in: text, range: NSRange(location: 0, length: text.length), withTemplate: "[Link]")
-        _ = try? NSRegularExpression(pattern: #"(http|https)://.*\W"#).replaceMatches(in: text, range: NSRange(location: 0, length: text.length), withTemplate: "[Link]")
-        
-        return String(text)
-    }
 }
