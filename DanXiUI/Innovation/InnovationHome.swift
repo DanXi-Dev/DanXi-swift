a3219f6bf6eb9905945edc41753bdf739af19c13
fsy2001 <fsy2001@outlook.com>
2025-04-05 01:04:02 +0800

    change: redesign webview toolbar

--- DanXiUI/Innovation/InnovationHome.swift
+++ DanXiUI/Innovation/InnovationHome.swift
@@ -1,9 +1,12 @@
 import SwiftUI
+import ViewUtils
 import DanXiKit
 import WebKit
 
 public struct InnovationHomePage: View {
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject private var navigator: AppNavigator
     @StateObject private var model = WebViewModel()
     
     public init() { }
@@ -11,155 +14,136 @@ public struct InnovationHomePage: View {
     public var body: some View {
         GeometryReader { proxy in
             ZStack {
-                WebView(frame: proxy.frame(in: .local))
+                InnovationWebView(url: URL(string: "https://danta.fudan.edu.cn/lobby/1")!, frame: proxy.frame(in: .local))
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .environmentObject(model)
-                    .opacity(model.loadingComplete ? 1 : 0)
                 
-                if !model.loadingComplete {
-                    VStack {
-                        ProgressView()
-                        Text("Loading", bundle: .module)
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                    }
-                }
-                
-                if let error = model.loadingError {
-                    VStack {
-                        Text("Loading Failed", bundle: .module)
-                            .font(.title)
-                            .fontWeight(.bold)
-                        
-                        Text(error.localizedDescription)
-                            .font(.callout)
-                            .padding(.bottom)
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.secondary)
-                        
-                        Button {
-                            model.reload()
-                        } label: {
-                            Text("Retry", bundle: .module)
-                        }
-                        .foregroundStyle(Color.accentColor)
-                    }
-                    .padding()
-                }
+                overlay
             }
         }
         .ignoresSafeArea(.all)
+        .navigationBarBackButtonHidden()
         .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                backButton
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                menu
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var overlay: some View {
+        switch model.loadingStatus {
+        case .completed:
+            EmptyView()
+        case .loading:
+            VStack {
+                ProgressView()
+                Text("Loading", bundle: .module)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+        case .failed(let error):
+            VStack {
+                Text("Loading Failed", bundle: .module)
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Text(error.localizedDescription)
+                    .font(.callout)
+                    .padding(.bottom)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.secondary)
+                
+                Button {
+                    model.reload()
+                } label: {
+                    Text("Retry", bundle: .module)
+                }
+                .foregroundStyle(Color.accentColor)
+            }
+            .padding()
+        }
+    }
+    
+    @ViewBuilder
+    private var backButton: some View {
+        Button {
+            if model.canGoBack {
+                model.goBack()
+                return
+            }
+            
+            if navigator.isCompactMode {
+                dismiss()
+            }
+        } label: {
+            Label {
+                Text("Back", bundle: .module)
+            } icon: {
+                Image(systemName: "chevron.left")
+            }
+        }
+        .disabled(!(model.canGoBack || navigator.isCompactMode)) // no need to go to home page in case of wide screen
+    }
+    
+    private var menu: some View {
+        Menu {
+            if navigator.isCompactMode {
+                Button {
+                   dismiss()
+                } label: {
+                    Label {
+                        Text("Community Home Page", bundle: .module)
+                    } icon: {
+                        Image(systemName: "house")
+                    }
+                }
+            }
+            
             Button {
                 model.goBack()
             } label: {
-                Image(systemName: "chevron.left")
+                Label {
+                    Text("Go Back", bundle: .module)
+                } icon: {
+                    Image(systemName: "arrow.left")
+                }
             }
             .disabled(!model.canGoBack)
             
             Button {
-                model.goForward()
+                model.goBack()
             } label: {
-                Image(systemName: "chevron.right")
+                Label {
+                    Text("Go Forward", bundle: .module)
+                } icon: {
+                    Image(systemName: "arrow.right")
+                }
             }
-            .disabled(!model.canGoForward)
+            .disabled(!model.canGoBack)
             
             Button {
                 model.reload()
             } label: {
-                Image(systemName: "arrow.clockwise")
+                Label {
+                    Text("Refresh", bundle: .module)
+                } icon: {
+                    Image(systemName: "arrow.clockwise")
+                }
             }
+        } label: {
+            Image(systemName: "ellipsis.circle")
         }
     }
 }
 
-private class WebViewModel: NSObject, ObservableObject {
-    @Published var canGoBack = false
-    @Published var canGoForward = false
-    @Published var loadingComplete = false
-    @Published var loadingError: Error? = nil
-    
-    weak var webview: WKWebView? = nil
-    
-    func goBack() {
-        webview?.goBack()
+#Preview {
+    NavigationStack {
+        InnovationHomePage()
     }
-    
-    func goForward() {
-        webview?.goForward()
-    }
-    
-    func reload() {
-        loadingComplete = false
-        loadingError = nil
-        
-        webview?.reload()
-    }
-}
-
-extension WebViewModel: WKNavigationDelegate {
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        loadingComplete = true
-    }
-    
-    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: any Error) {
-        loadingError = error
-        loadingComplete = true
-    }
-}
-
-private struct WebView: UIViewRepresentable {
-    @EnvironmentObject private var model: WebViewModel
-    
-    typealias Coordinator = WebViewModel
-    
-    let frame: CGRect
-    
-    func makeCoordinator() -> WebViewModel {
-        model
-    }
-    
-    func makeUIView(context: Context) -> WKWebView {
-        let config = WKWebViewConfiguration()
-        config.processPool = WKProcessPool()
-        
-        if let token = CredentialStore.shared.token {
-            let accessCookie = HTTPCookie(properties: [
-                .domain: "danta.fudan.edu.cn",
-                .path: "/",
-                .name: "access",
-                .value: token.access,
-                .version: 1,
-            ])
-            
-            let refreshCookie = HTTPCookie(properties: [
-                .domain: "danta.fudan.edu.cn",
-                .path: "/",
-                .name: "refresh",
-                .value: token.refresh,
-                .version: 1,
-            ])
-            
-            if let accessCookie, let refreshCookie {
-                config.websiteDataStore.httpCookieStore.setCookie(accessCookie)
-                config.websiteDataStore.httpCookieStore.setCookie(refreshCookie)
-            }
-        }
-        
-        let webView = WKWebView(frame: frame, configuration: config)
-        let url = URL(string: "https://danta.fudan.edu.cn/lobby/1")!
-        let request = URLRequest(url: url)
-        webView.allowsBackForwardNavigationGestures = true
-        webView.load(request)
-        webView.navigationDelegate = context.coordinator
-        context.coordinator.webview = webView
-        return webView
-    }
-    
-    func updateUIView(_ webView: WKWebView, context: Context) {
-        DispatchQueue.main.async {
-            context.coordinator.canGoBack = webView.canGoBack
-            context.coordinator.canGoForward = webView.canGoForward
-        }
-    }
+    .environmentObject(AppNavigator())
 }
---
+++ DanXiUI/Innovation/InnovationWebView.swift
@@ -0,0 +1,107 @@
+import SwiftUI
+import WebKit
+import DanXiKit
+
+// MARK: - UI
+
+struct InnovationWebView: UIViewRepresentable {
+    @EnvironmentObject private var model: WebViewModel
+    
+    typealias Coordinator = WebViewModel
+    
+    let url: URL
+    let frame: CGRect
+    
+    func makeCoordinator() -> WebViewModel {
+        model
+    }
+    
+    private func setupConfiguration() -> WKWebViewConfiguration {
+        let configuration = WKWebViewConfiguration()
+        configuration.processPool = WKProcessPool()
+        
+        if let token = CredentialStore.shared.token {
+            let accessCookie = HTTPCookie(properties: [
+                .domain: "danta.fudan.edu.cn",
+                .path: "/",
+                .name: "access",
+                .value: token.access,
+                .version: 1,
+            ])
+            
+            let refreshCookie = HTTPCookie(properties: [
+                .domain: "danta.fudan.edu.cn",
+                .path: "/",
+                .name: "refresh",
+                .value: token.refresh,
+                .version: 1,
+            ])
+            
+            if let accessCookie, let refreshCookie {
+                configuration.websiteDataStore.httpCookieStore.setCookie(accessCookie)
+                configuration.websiteDataStore.httpCookieStore.setCookie(refreshCookie)
+            }
+        }
+        
+        return configuration
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let configuration = setupConfiguration()
+        let webview = WKWebView(frame: frame, configuration: configuration)
+        let request = URLRequest(url: url)
+        webview.allowsBackForwardNavigationGestures = true
+        webview.load(request)
+        webview.navigationDelegate = context.coordinator
+        context.coordinator.webview = webview
+        return webview
+    }
+    
+    func updateUIView(_ webview: WKWebView, context: Context) {
+        DispatchQueue.main.async {
+            context.coordinator.canGoBack = webview.canGoBack
+            context.coordinator.canGoForward = webview.canGoForward
+        }
+    }
+    
+}
+
+// MARK: - Data
+
+enum WebViewLoadingStatus {
+    case loading
+    case completed
+    case failed(error: Error)
+}
+
+class WebViewModel: NSObject, ObservableObject {
+    @Published var loadingStatus = WebViewLoadingStatus.loading
+    @Published var canGoBack = false
+    @Published var canGoForward = false
+    weak var webview: WKWebView! = nil
+    
+    func goBack() {
+        webview.goBack()
+    }
+    
+    func goForward() {
+        webview.goForward()
+    }
+    
+    func reload() {
+        loadingStatus = .loading
+        webview.reload()
+    }
+}
+
+extension WebViewModel: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("completed")
+        loadingStatus = .completed
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: any Error) {
+        print("failed")
+        loadingStatus = .failed(error: error)
+    }
+}
