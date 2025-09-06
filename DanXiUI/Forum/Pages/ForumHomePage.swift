71c13739f5f50dbaadfcb7ebf00bd71702d68694
Ivanfei <ivanfei2218@icloud.com>
2025-09-07 03:12:40 +0800

    change: ForumHomePage loading changed to sequential

--- DanXiUI/Forum/Pages/ForumHomePage.swift
+++ DanXiUI/Forum/Pages/ForumHomePage.swift
@@ -5,41 +5,25 @@ import DanXiKit
 
 struct ForumHomePage: View {
     func loadAll() async throws {
-        let divisionTask = Task {
-            if !DivisionStore.shared.initialized {
-                try await DivisionStore.shared.refreshDivisions()
-            }
+        if !DivisionStore.shared.initialized {
+            try await DivisionStore.shared.refreshDivisions()
         }
         
-        let favoriteTask = Task {
-            if !FavoriteStore.shared.initialized {
-                try await FavoriteStore.shared.refreshFavoriteIds()
-            }
+        if !FavoriteStore.shared.initialized {
+            try await FavoriteStore.shared.refreshFavoriteIds()
         }
         
-        let profileTask = Task {
-            if !ProfileStore.shared.initialized {
-                try await ProfileStore.shared.refreshProfile()
-            }
+        if !ProfileStore.shared.initialized {
+            try await ProfileStore.shared.refreshProfile()
         }
         
-        let subscriptionTask = Task {
-            if !SubscriptionStore.shared.initialized {
-                try await SubscriptionStore.shared.refreshSubscriptionIds()
-            }
+        if !SubscriptionStore.shared.initialized {
+            try await SubscriptionStore.shared.refreshSubscriptionIds()
         }
         
-        let tagTask = Task {
-            if !TagStore.shared.initialized {
-                try await TagStore.shared.refreshTags()
-            }
+        if !TagStore.shared.initialized {
+            try await TagStore.shared.refreshTags()
         }
-        
-        try await divisionTask.value
-        try await favoriteTask.value
-        try await profileTask.value
-        try await subscriptionTask.value
-        try await tagTask.value
     }
     
     var body: some View {
