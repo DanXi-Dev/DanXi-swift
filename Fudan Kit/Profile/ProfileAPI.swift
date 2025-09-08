de4f575df7712db60477417685b9bbe8ec8fc03d
m4-mac <yxyhaha5@gmail.com>
2025-09-08 11:14:51 +0800

    fix: ProfileAPI migrated to new authentication (#502)

--- Fudan Kit/Profile/ProfileAPI.swift
+++ Fudan Kit/Profile/ProfileAPI.swift
@@ -2,9 +2,11 @@ import Foundation
 
 /// API for getting student profile
 public enum ProfileAPI {
+    static let loginURL = URL(string: "https://workflow1.fudan.edu.cn/site/login/cas-login?redirect_url=https%3A%2F%2Fworkflow1.fudan.edu.cn%2Fv2%2Fsite%2Findex")!
+    
     public static func getStudentProfile() async throws -> Profile {
         let url = URL(string: "https://workflow1.fudan.edu.cn/site/fudan/student-information")!
-        let data = try await Authenticator.classic.authenticate(url)
+        let data = try await Authenticator.neo.authenticate(url, loginURL: loginURL)
         let json = try unwrapJSON(data)
         let content = try json["info"].rawData()
         let response = try JSONDecoder().decode(ProfileResponse.self, from: content)
